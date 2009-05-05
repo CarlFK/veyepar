fb65d373c4d80dbf1622a247e239da1865935a33
Carl Karsten <carl@gw42.(none)>
2009-05-05 15:54:02 -0500

    i can't remember...

--- dj/main/models.py
+++ dj/main/models.py
@@ -2,13 +2,13 @@
 
 from django.db import models
 
-class Show(models.Model):
+class Client(models.Model):
     sequence = models.IntegerField(default=1)
     name = models.CharField(max_length=135)
     slug = models.CharField(max_length=135,help_text="dir name to store input files")
 
-class Season(models.Model):
-    show = models.ForeignKey(Show)
+class Show(models.Model):
+    client = models.ForeignKey(Client)
     sequence = models.IntegerField(default=1)
     name = models.CharField(max_length=135)
 
@@ -18,32 +18,33 @@ class Location(models.Model):
     name = models.CharField(max_length=135,help_text="room name")
     slug = models.CharField(max_length=135,help_text="dir name to store input files")
 
-class Raw_File(models.Model):
-    location = models.ForeignKey(Location)
-    filename = models.CharField(max_length=135,help_text="pathname.dv")
-    start = models.DateTimeField(null=True, blank=True)
-    end = models.DateTimeField(null=True, blank=True)
-    comment = models.TextField(blank=True)
-
 class Quality(models.Model):
     level = models.IntegerField()
     name = models.CharField(max_length=35)
     description = models.TextField(blank=True)
 
+class Raw_File(models.Model):
+    location = models.ForeignKey(Location)
+    filename = models.CharField(max_length=135,help_text="pathname.dv")
+    start = models.DateTimeField(null=True, blank=True, 
+        help_text='when recorded (should agree with file name and timestamp)')
+    end = models.DateTimeField(null=True, blank=True)
+    comment = models.TextField(blank=True)
+
 class Episode(models.Model):
-    season = models.ForeignKey(Season)
     location = models.ForeignKey(Location)
     sequence = models.IntegerField(help_text="process order")
     primary = models.CharField(max_length=135,
         help_text="pointer to master version of event (name,desc,time,author,files,etc)")
-    name = models.CharField(max_length=135)
-    slug = models.CharField(max_length=135,help_text="root file name")
-    description = models.TextField(blank=True)
-    start = models.DateTimeField(null=True, blank=True)
+    name = models.CharField(max_length=135, help_text="(synced from primary source)")
+    description = models.TextField(blank=True, help_text="(synced from primary source)")
+    slug = models.CharField(max_length=135,help_text="used for file name")
+    start = models.DateTimeField(null=True, blank=True, 
+        help_text="initially scheduled time from master, adjusted to match reality")
     end = models.DateTimeField(null=True, blank=True)
     video_quality = models.ForeignKey(Quality,related_name='video_quality')
     audio_quality = models.ForeignKey(Quality,related_name='audio_quality')
-    comment = models.TextField(blank=True)
+    comment = models.TextField(blank=True, help_text="production notes")
 
 class Cut_List(models.Model):
     raw_file = models.ForeignKey(Raw_File)
