18569a6d8cead9ae9d5a5191d35333b2b8ab9ce2
Joe User <juser@dhcp216.personnelware.com>
2009-05-18 21:54:04 -0500

    it works

--- dj/main/admin.py
+++ dj/main/admin.py
@@ -5,16 +5,19 @@ from main.models import Show, Season, Location, Raw_File, Quality, Episode, Cut_
 class ShowAdmin(admin.ModelAdmin):
     list_display = ('sequence', 'name',)
     admin_order_field = ('sequence', 'name',)
+    prepopulated_fields = {"slug": ("name",)}
 admin.site.register(Show, ShowAdmin)
 
 class SeasonAdmin(admin.ModelAdmin):
-    list_display = ('sequence', 'name',)
+    list_display = ('sequence', 'show_name', 'name',)
     admin_order_field = ('sequence', 'name',)
 admin.site.register(Season, SeasonAdmin)
 
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('sequence', 'name',)
     admin_order_field = ('sequence', 'name',)
+    prepopulated_fields = {"slug": ("name",)}
+admin.site.register(Location, LocationAdmin)
 
 class Raw_FileAdmin(admin.ModelAdmin):
     list_display = ('filename','start', 'end') 
@@ -24,12 +27,13 @@ admin.site.register(Raw_File, Raw_FileAdmin)
 class QualityAdmin(admin.ModelAdmin):
     list_display = ('level', 'name',)
     admin_order_field = ('level', 'name',)
-    list_editable = list_display
+    # list_editable = list_display
 admin.site.register(Quality, QualityAdmin)
 
 class EpisodeAdmin(admin.ModelAdmin):
     list_display = ('sequence', 'name',)
     admin_order_field = ('sequence', 'name',)
+    prepopulated_fields = {"slug": ("name",)}
 admin.site.register(Episode, EpisodeAdmin)
 
 class Cut_ListAdmin(admin.ModelAdmin):
--- dj/main/models.py
+++ dj/main/models.py
@@ -6,17 +6,26 @@ class Show(models.Model):
     sequence = models.IntegerField(default=1)
     name = models.CharField(max_length=135)
     slug = models.CharField(max_length=135,help_text="dir name to store input files")
+    def __unicode__(self):
+        return self.name
 
 class Season(models.Model):
     show = models.ForeignKey(Show)
     sequence = models.IntegerField(default=1)
     name = models.CharField(max_length=135)
+    @property
+    def show_name(self):
+        return self.show
+    def __unicode__(self):
+        return "%s: %s" % ( self.show_name, self.name )
 
 class Location(models.Model):
     sequence = models.IntegerField(default=1)
-    show = models.ForeignKey(Show)
+    show = models.ForeignKey(Season)
     name = models.CharField(max_length=135,help_text="room name")
     slug = models.CharField(max_length=135,help_text="dir name to store input files")
+    def __unicode__(self):
+        return self.name
 
 class Raw_File(models.Model):
     location = models.ForeignKey(Location)
@@ -24,16 +33,21 @@ class Raw_File(models.Model):
     start = models.DateTimeField(null=True, blank=True)
     end = models.DateTimeField(null=True, blank=True)
     comment = models.TextField(blank=True)
+    def __unicode__(self):
+        return self.filename
 
 class Quality(models.Model):
     level = models.IntegerField()
     name = models.CharField(max_length=35)
     description = models.TextField(blank=True)
+    def __unicode__(self):
+        return self.name
 
 class Episode(models.Model):
     season = models.ForeignKey(Season)
-    location = models.ForeignKey(Location)
-    sequence = models.IntegerField(help_text="process order")
+    location = models.ForeignKey(Location, null=True)
+    sequence = models.IntegerField(null=True,
+        help_text="process order")
     primary = models.CharField(max_length=135,
         help_text="pointer to master version of event (name,desc,time,author,files,etc)")
     name = models.CharField(max_length=135)
@@ -44,6 +58,8 @@ class Episode(models.Model):
     video_quality = models.ForeignKey(Quality,related_name='video_quality')
     audio_quality = models.ForeignKey(Quality,related_name='audio_quality')
     comment = models.TextField(blank=True)
+    def __unicode__(self):
+        return self.name
 
 class Cut_List(models.Model):
     raw_file = models.ForeignKey(Raw_File)
@@ -54,6 +70,8 @@ class Cut_List(models.Model):
     end = models.CharField(max_length=11, blank=True,
         help_text='offset from start in HH:MM:SS.SS')
     comment = models.TextField(blank=True)
+    def __unicode__(self):
+        return "%s - %s" % (self.start, self.end)
 
 class State(models.Model):
     sequence = models.IntegerField(default=1)
