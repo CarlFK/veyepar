9f46a018c70d71239afdb69b60e9d7775b6ce984
Carl Karsten <carl@gw42.(none)>
2009-08-19 19:23:49 -0500

    ocr and timestamps

---
+++ dj/scripts/dvdate.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+# dvdate.py - gets date of .dv file - shells out to dvgrab
+
+import os, subprocess
+import time
+from dateutil.parser import parse
+
+dvfile = 'pyohio.dv'
+
+def get_timestamp(dvfilename):
+    while True:
+        p = subprocess.Popen( ['dvgrab', '-d', '1', '-I', dvfilename, '/tmp/dvg'], stderr=subprocess.PIPE ) 
+        time.sleep(1)
+        if p.poll() is None:
+            print "die!"
+            # p.send_signal()
+            p.kill()
+            print "dead?"
+        else:
+            out,err = p.communicate()
+            break
+
+    try:
+        text = err.split('\n')
+        for info in text:
+            if "timecode" in info: break
+        # pos = text.index('Capture Started')+1 # 2 or 3 depending on if 'End of pipe' sneaked in
+        info = info.split()
+        pos = info.index('date')+1 # +1 becaue the date/time come after it
+    except:
+        print out, err
+        print text
+        raise
+    dt=info[pos:pos+2]
+    print dt
+    dt="%s %s" % tuple(dt)
+# prolly use:
+# datetime.strptime(date_string, format)
+    dt=parse(dt)
+    return dt
+
+if __name__=='__main__':
+    dvfile = 'pyohio.dv' 
+    dt = get_timestamp(dvfile)
+    print dt
+    ts = time.mktime(dt.timetuple()) 
+    print ts
+    # os.utime(path, times) Set the access and modified 
+    # os.utime(dvfile,(ts,ts))
+
---
+++ dj/scripts/ocrdv.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+
+# ocrdv - reads frames from a .dv untill it finds a bunch of words
+
+import subprocess
+import pyffmpeg
+from cStringIO import StringIO
+from csv import DictReader
+from collections import defaultdict
+
+def Titwords():
+    reader = DictReader(open("sched.csv", "rb"))
+    titwords=defaultdict(list)
+    for row in reader:
+        title=row['title']
+        for w in title.split():
+            w = ''.join([c for c in w if c.isalpha()])
+            if len(w)>3: 
+                titwords[w.lower()].append(title)
+#  re.compile('[^a-z]').sub('', 'foo:')
+    return titwords
+
+def Dictionary():
+    dictionary = [w for w in open('dictionary.txt').read().split() if len(w)>3]
+    return dictionary
+
+def Score(ocrtext):
+    score = 0
+    words = ocrtext.split()
+    titl=[]
+    for w in words:
+        # strip all non chars
+        w = ''.join([c for c in w if c.isalpha()]).lower()
+        if len(w)>3 and titwords.get(w):
+            tits = titwords[w]
+            titl.append(tits)
+            score+=len(tits)
+
+    dictwords = [w for w in words if len(w)>3 and w.upper() in dictionary]
+    if dictwords:
+        titl.append(dictwords)
+        score+=len(dictwords)
+
+    return score, titl
+
+titwords = Titwords()
+dictionary = Dictionary()
+
+dvfn = '/home/carl/Videos/pyohio/2009-07-25/auditorium/pyohio-4.dv'
+
+def ocrdv(dvfn,maxframes):
+    
+    stream = pyffmpeg.VideoStream()
+    stream.open(dvfn)
+    frameno=0
+    lastocr = ''
+    while frameno < maxframes:
+
+        image = stream.GetFrameNo(frameno)
+
+        # get PPM image from PIL image 
+        buffer = StringIO()
+        image.save(buffer,'PPM')
+        img = buffer.getvalue()
+        buffer.close()
+
+        # ocr the image
+        p = subprocess.Popen(['gocr', '-'],
+          stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        ocrtext, stderrdata = p.communicate(img)
+        if stderrdata: print "ERR:", stderrdata
+
+        if ocrtext != lastocr:
+           
+            # score the text
+            print "GOCR found:\n", ocrtext
+            score,titls = Score(ocrtext)
+            if score>2:
+                print "score", score
+                print titls
+                return ocrtext
+
+        frameno+=30*60  # bump on min
+
+    return ''
