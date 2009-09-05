d3df899ad6292dcde5e2c648fd6ccc775a933a1d
Joe User <juser@dhcp216.personnelware.com>
2009-09-05 12:24:42 -0500

    uses process

--- dj/scripts/mkdirs.py
+++ dj/scripts/mkdirs.py
@@ -3,50 +3,27 @@
 # Makes the dir tree to put files into
 
 import  os,sys
-import optparse
 
-sys.path.insert(0, '..' )
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-import settings
-settings.DATABASE_NAME="../vp.db"
+from process import process
 
 from main.models import Client, Show, Location
 
-def mkdirs(root,client,show):
-    root = "%s/%s/%s" % (root, client.slug, show.slug)
-    print "%s/dv" % (root,)
-    os.makedirs("%s/dv" % (root,))
-    locs = Location.objects.filter(show=show)
-    for loc in locs:
-         dir="%s/dv/%s" % (root,loc.slug)
-         print dir
-         os.mkdir(dir)
+class mkdirs(process):
 
-def parse_args():
-    parser = optparse.OptionParser()
-    parser.add_option('-s', '--show' )
-    parser.add_option('-c', '--client' )
-    parser.add_option('-r', '--root', default='/home/carl/Videos/veyepar' )
-    parser.add_option('-l', '--list', action="store_true" )
+  def mkdir(self,dir):
+      """ makes the dir if it doesn't exist """
+      print dir
+      if not os.path.exists(dir):
+         os.makedirs(dir)
 
-    options, args = parser.parse_args()
-    return options, args
+  def one_show(self,show):
+    self.mkdir(os.path.join(self.show_dir,'dv'))
+    self.mkdir(os.path.join(self.show_dir,'ogg'))
+    for loc in Location.objects.filter(show=show):
+         dir = os.path.join(self.show_dir,'dv',loc.slug)
+         self.mkdir(dir)
 
+if __name__=='__main__': 
+    p=mkdirs()
+    p.main()
 
-def main():
-    options, args = parse_args()
-
-    if options.list:
-        for client in Client.objects.all(): 
-            print "\nName: %s  Slug: %s" %( client.name, client.slug )
-            for show in Show.objects.filter(client=client):
-                print "\tName: %s  Slug: %s" %( show.name, show.slug )
-                print "\t--client %s --show %s" %( client.slug, show.slug )
-    else:    
-        client = Client.objects.get(slug=options.client)
-        show = Show.objects.get(client=client,slug=options.show)
-        root = options.root
-        print root, client, show
-        mkdirs( root, client, show )
-
-if __name__=='__main__': main()
