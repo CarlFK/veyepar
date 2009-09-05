74ddde5ba7aff9fa72f6a2a7b42e52591c59ea43
Joe User <juser@dhcp216.personnelware.com>
2009-09-05 13:25:33 -0500

    plane landing

---
+++ dj/scripts/tsdv.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+"""
+tsdv.py - timestamp dv
+sets start/end times of dv files
+
+Gets start from one of:
+the file system time stamp, 
+the first frame of the dv
+the file name (assumes hh:mm:ss.dv format)
+
+Gets end from:
+
+"""
+
+
+import  os
+
+from process import process
+
+from main.models import Client, Show, Location, Episode, Raw_File, Cut_List
+
+class add_dv(process):
+
+# Raw_File.objects.filter(location__show=show).delete()
+
+    def one_dir(self, location, dir):
+    
+        files=os.listdir(dir)
+        seq=0
+        for dv in [f for f in files if f[-3:]=='.dv']:
+            seq+=1
+            # print dv
+            pathname = os.path.join(dir,dv)
+            print pathname
+            rf, created = Raw_File.objects.get_or_create(
+                location=location,
+                filename=dv,)
+            if created: 
+                rf.sequence=seq
+                rf.save()
+
+
+    def one_loc(self,location,dir):
+      files=os.listdir(dir)
+      print files
+      for dt in files:
+        # dt is typicaly a date looking thing: 2009-08-20
+        dir = os.path.join(dir,dt) 
+        print (location,dir)
+        self.one_dir(location,dir)
+
+    def one_show(self, show):
+      for loc in Location.objects.filter(show=show):
+        dir=os.path.join(self.show_dir,'dv',loc.slug)
+        print show,loc,dir
+        self.one_loc(loc, dir)
+
+if __name__=='__main__': 
+    p=add_dv()
+    p.main()
+
