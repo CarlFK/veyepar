71fe9e04443226c3380110e983a40b371b057ece
Carl Karsten <carl@gw42.(none)>
2009-09-04 20:42:13 -0500

    abstracted the episode selection into process.py.  neet to apply to other scripts.

--- dj/scripts/post.py
+++ dj/scripts/post.py
@@ -7,37 +7,31 @@ from blip_uploader import Upload
 import tweeter
 import optparse
 import re
-import os,sys
+import os
 
 import pw
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-sys.path.insert(0, '..' )
-import settings
-settings.DATABASE_NAME="../vp.db"
+from process import process
 
 from main.models import Show, Location, Episode, Raw_File, Cut_List
 
-def do_one(ep):
+class post(process):
+
+  ready_state = 3
+  done_state = 4
+
+  def process_ep(self, ep):
     print ep.id, ep.name
     loc = ep.location
     show = loc.show
     client = show.client
 
-    # dir the show is in
-    root=os.path.join('/home/carl/Videos/veyepar',client.slug,show.slug)
-
-    # dir: source and thumbs
-    dt=ep.start.strftime("%Y-%m-%d")
-    dir=os.path.join(root,'dv',loc.slug,dt)
-
     # look for a thumb
     for cut in Cut_List.objects.filter(episode=ep).order_by('sequence'):
         basename = cut.raw_file.basename()        
-        thumb=os.path.join(dir, "%s.png"%(basename))
+        thumb=os.path.join(self.episode_dir, "%s.png"%(basename))
         if os.path.exists(thumb): break
     
-    oggpathname = os.path.join( root, "ogg", "%s.ogg"%(ep.slug) )
+    oggpathname = os.path.join( self.show_dir, "ogg", "%s.ogg"%(ep.slug) )
     description = "%s</br>\n</br>\n%s" % (ep.description, client.description)
 
     print description 
@@ -45,12 +39,14 @@ def do_one(ep):
     meta = {
         'title': ep.name,
         'description': description,
-        "topics": "%s, %s, python, pycon, conference, ohio, 2009"%(show.name,show.client.name),
+        "topics": "%s, python, "%(client.name),
         "license": "13",
         "categories_id": "10",
         }
 
     print oggpathname, thumb
+    print meta
+    return 
 
     # blipurl = "http://carlfk.blip.tv/file/2213225"
     response = Upload("", pw.blip['user'], pw.blip['password'], oggpathname, meta, thumb)
@@ -64,54 +60,13 @@ def do_one(ep):
         tweetid=re.search("<id>(.*)</id>" ,tweet).groups()[0]
         tweeturl="http://twitter.com/cfkarsten/status/%s"%(tweetid,)
         print tweeturl
-        ep.state = 4
         ep.comment += blipurl
+        ep.state = self.done_state
     else:
         ep.comment += "upload failed\n"
     ep.save()
 
-def do_eps(episodes):
-    for ep in episodes:
-        if ep.state==3:
-             # print ep.id, ep.name
-             do_one(ep)
-
-def do_show(show):
-    locs = Location.objects.filter(show=show)
-    for loc in locs:
-        episodes = Episode.objects.filter(location=loc,state=2)
-        do_eps(episodes)
-
-
-def parse_args():
-    parser = optparse.OptionParser()
-    parser.add_option('-a', '--all' )
-    parser.add_option('-s', '--show' )
-    parser.add_option('-d', '--day' )
-
-    options, args = parser.parse_args()
-    return options, args
-
-
-def main():
-    options, args = parse_args()
-
-    if options.all:
-        show = Show.objects.get(name='PyOhio09')
-        do_show(show)
-    elif options.show:
-        show = Show.objects.get(name=options.show)
-        d0_show(show)
-    elif options.day:
-        show = Show.objects.get(name='PyOhio09')
-        episodes = Episode.objects.filter(location__show=show,start__day=options.day)
-        do_eps(episodes)
-    else:
-        episodes = Episode.objects.filter(id__in=args)
-        do_eps(episodes)
-
-    
-
 if __name__ == '__main__':
-    main()
+    p=post()
+    p.main()
 
---
+++ dj/scripts/process.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+
+# abstract class for processing episodes
+
+import optparse
+import os,sys,subprocess
+import datetime
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+sys.path.insert(0, '..' )
+import settings
+settings.DATABASE_NAME="../vp.db"
+
+from main.models import Client, Show, Location, Episode 
+
+class process(object):
+
+  ready_state = None
+  done_state = None
+
+  root_dir='/home/carl/Videos/veyepar' 
+  show_dir=None
+  episode_dir=None
+
+  def process_ep(self, episode):
+    print episode.id, episode.name
+    return 
+
+  def process_eps(self, episodes):
+    for ep in episodes:
+        if ep.state==self.ready_state:
+            self.episode_dir=os.path.join( self.show_dir, 'dv', 
+                ep.location.slug, ep.start.strftime("%Y-%m-%d") )
+            self.process_ep(ep)
+
+  def one_show(self, show):
+    locs = Location.objects.filter(show=show)
+    for loc in locs:
+        episodes = Episode.objects.filter(location=loc,state=self.ready_state)
+        self.process_eps(episodes)
+
+  def parse_args(self):
+    parser = optparse.OptionParser()
+    parser.add_option('-c', '--client' )
+    parser.add_option('-s', '--show' )
+    parser.add_option('-d', '--day' )
+    parser.add_option('-l', '--list', action="store_true" )
+
+    options, args = parser.parse_args()
+    return options, args
+
+  def main(self):
+    options, args = self.parse_args()
+
+    if options.list:
+        for client in Client.objects.all():
+            print "\nName: %s  Slug: %s" %( client.name, client.slug )
+            for show in Show.objects.filter(client=client):
+                print "\tName: %s  Slug: %s" %( show.name, show.slug )
+                print "\t--client %s --show %s" %( client.slug, show.slug )
+                for ep in Episode.objects.filter(location__show=show):
+                    print "\t\t id: %s state: %s %s" % ( 
+                        ep.id, ep.state, ep.name )
+    elif options.client and options.show:
+        client = Client.objects.get(slug=options.client)
+        show = Show.objects.get(client=client,slug=options.show)
+        self.show_dir = os.path.join(self.root_dir,client.slug,show.slug)
+        self.one_show(show)
+
+    elif options.day:
+        show = Show.objects.get(name='PyOhio09')
+        episodes = Episode.objects.filter(location__show=show,start__day=options.day)
+        enc_eps(episodes)
+    else:
+        episodes = Episode.objects.filter(id__in=args)
+        self.process_eps(episodes)
+    
+
+if __name__ == '__main__':
+    p=process()
+    p.main()
+
