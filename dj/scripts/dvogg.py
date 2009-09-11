ffec00ddc658fffa34e8977086c880219a2be698
Joe User <juser@dhcp216.personnelware.com>
2009-09-11 07:41:02 -0500

    doesen't reencode

--- dj/scripts/dvogg.py
+++ dj/scripts/dvogg.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python
-
-# makes .ogg for all dv in a show
+#!/usr/bin/python # makes .ogg for all dv in a show
 
 import  os
 import subprocess
@@ -14,10 +12,11 @@ class mkpreview(process):
     def one_file(self,loc_dir,rf):
         src = os.path.join(loc_dir,rf.filename)
         dst = os.path.join(loc_dir,rf.basename()+'.ogg')
-        cmd="ffmpeg2theora --videoquality 1 --audioquality 3 --audiobitrate 48 --speedlevel 2 --width 360 --height 240 --framerate 2 --keyint 256 --channels 1".split()
-        cmd+=[ '-o', dst, src ]
-        print ' '.join(cmd)
-        p=subprocess.Popen(cmd).wait()
+        if not os.path.exiests(dst):
+            cmd="ffmpeg2theora --videoquality 1 --audioquality 3 --audiobitrate 48 --speedlevel 2 --width 360 --height 240 --framerate 2 --keyint 256 --channels 1".split()
+            cmd+=[ '-o', dst, src ]
+            print ' '.join(cmd)
+            p=subprocess.Popen(cmd).wait()
    
     def one_loc(self,location):
       """
