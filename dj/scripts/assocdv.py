69dbb8d7fa98d290410a0abd934c577b4832a915
Carl Karsten <carl@gw42.(none)>
2009-10-02 21:35:34 -0500

    one orpahn event per room per day

--- dj/scripts/assocdv.py
+++ dj/scripts/assocdv.py
@@ -4,13 +4,13 @@
 
 import os
 
-from process import process
+import process
 
 from django.db.models import Q
 
-from main.models import Client, Show, Location, Episode, Raw_File, Cut_List
+# from main.models import Client, Show, Location, Episode, Raw_File, Cut_List
 
-class ass_dv(process):
+class ass_dv(process.process):
 
     def one_dv(self, dv, seq ):
         print dv, dv.location
@@ -18,18 +18,24 @@ class ass_dv(process):
             dv.location.name,
             dv.start.strftime('%a_%d'),
             )
-        orph_slug = process.fnify(orph_slug)
+        orph_slug = process.fnify(orph_name)
         # find Episodes this may be a part of, add a cutlist record
         eps = Episode.objects.filter(
             Q(start__lte=dv.end)|Q(start__isnull=True), 
             Q(end__gte=dv.start)|Q(end__isnull=True), 
-            location=dv.location).exclude(slug=orph_slug )
+            location=dv.location).exclude(slug='orphans').exclude(slug=orph_slug )
         if not eps:
             # if no episodes found, attach to the orphan bucket
+            dvdate=dv.start.date()  # date the clip started on 
+            start = datetime.datetime.combine(dvdate,datetime.time(0))
+            end = datetime.datetime.combine(dvdate,datetime.time(23,59,59))
             ep,created=Episode.objects.get_or_create(
                name=orph_name,slug=orph_slug,
+               start=start, end=end,
                location=dv.location)
             eps=[ep]
+        else: eps=[] # only add orphans now.  hack atack.
+
         for ep in eps:
             print ep
             cl, created = Cut_List.objects.get_or_create(
