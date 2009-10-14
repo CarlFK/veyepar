ca025d9da29aee6652337aadc8c3064c85e011df
Carl Karsten <carl@gw42.(none)>
2009-10-14 11:07:21 -0500

    moved topics,cat,license to command line

--- dj/scripts/post.py
+++ dj/scripts/post.py
@@ -14,15 +14,16 @@ from process import process
 
 from main.models import Show, Location, Episode, Raw_File, Cut_List
 
+"""
 class Blip_Ep(blip_uploader.Blip):
 
     def progress(self, current, total):
-        """
+        " ""
         Displaies upload percent done, bytes sent, total bytes.
-        """
+        " ""
         sys.stdout.write('\r%3i%%  %s of %s bytes'
             % (100*current/total, current, total))
-
+"""
 
 class post(process):
 
@@ -46,35 +47,78 @@ class post(process):
 
     print description 
 
+    blip_cli=blip_uploader.Blip_CLI()
+
     meta = {
         'title': ep.name,
         'description': description,
+        }
+    """
+        # need to make a place for this stuff in the database.
         "topics": "%s, python, "%(client.name),
         "license": "13",
         "categories_id": "10",
-        }
+    """
+
+    if self.options.topics:
+        meta['topics'] = options.topics
+
+    if self.options.license:
+        if options.license=='list':
+            blip_cli.List_Licenses()
+            return
+        else:
+            meta['license'] = options.license
+
+    if options.category:
+        if options.category=='list':
+            self.List_Categories()
+            return
+        else:
+            meta['categorie_id'] = options.category
+
 
     print oggpathname, thumb
 
-    blip_ep=Blip_Ep()
-    response = blip_ep.Upload(
-        "", pw.blip['user'], pw.blip['password'], oggpathname, meta, thumb)
-    responsexml = response.read()
-    blipurl = re.search("post_url>(.*)</post" ,responsexml).groups()[0]
-    if blipurl:
-        print blipurl
-        prefix = "#%s VIDEO -" % show.client.name
-        tweet = tweeter.notify(prefix, ep.name, blipurl)
-        print tweet
-        if "<id>" not in tweet: print tweet
-        tweetid=re.search("<id>(.*)</id>" ,tweet).groups()[0]
-        tweeturl="http://twitter.com/cfkarsten/status/%s"%(tweetid,)
-        print tweeturl
-        ep.comment += blipurl
-        ep.state = self.done_state
+    # blip_ep=Blip_Ep()
+    if self.options.test:
+        print 'test mode:'
+        print 'blip_cli.Upload( "", user, pw, oggpathname, meta, thumb)'
+        print 'oggpathname %s' % oggpathname
+        print 'meta %s' % meta
+        print 'thumb %s' % thumb
+        print
     else:
-        ep.comment += "upload failed\n"
-    ep.save()
+        response = blip_ep.Upload(
+            "", pw.blip['user'], pw.blip['password'], oggpathname, meta, thumb)
+        responsexml = response.read()
+        blipurl = re.search("post_url>(.*)</post" ,responsexml).groups()[0]
+        if blipurl:
+            print blipurl
+            prefix = "#%s VIDEO -" % show.client.name
+            tweet = tweeter.notify(prefix, ep.name, blipurl)
+            print tweet
+            if "<id>" not in tweet: print tweet
+            tweetid=re.search("<id>(.*)</id>" ,tweet).groups()[0]
+            tweeturl="http://twitter.com/cfkarsten/status/%s"%(tweetid,)
+            print tweeturl
+            ep.comment += blipurl
+            ep.state = self.done_state
+        else:
+            ep.comment += "upload failed\n"
+        ep.save()
+
+    def add_more_options(self, parser):
+        parser.add_option('-T', '--topics',
+            help="list of topics (user defined)")
+        parser.add_option('-L', '--license',
+            help="13 is Creative Commons Attribution-NC-ShareAlike 3.0\n"
+            "'./blip_uploader.py -L list' to see full list" )
+        parser.add_option('-C', '--category',
+            help = "'./blip_uploader.py -C list' to see full list" )
+        parser.add_option('--hidden',
+            help="availability on blip.tv, 0=Available, 1=Hidden, 2=Available to family, 4=Available to friends/family.")
+
 
 if __name__ == '__main__':
     p=post()
