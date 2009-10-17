e602d22de73edd1a69412c5fb5bb558cbe970b88
Carl Karsten <carl@gw42.(none)>
2009-10-17 10:06:05 -0500

    typo

--- dj/scripts/dvogg.py
+++ dj/scripts/dvogg.py
@@ -14,9 +14,9 @@ class mkpreview(process):
     def one_file(self,loc_dir,rf):
         src = os.path.join(loc_dir,rf.filename)
         dst = os.path.join(loc_dir,rf.basename()+'.ogg')
-        if not os.path.exiests(dst):
+        if not os.path.exists(dst):
             cmd="ffmpeg2theora --videoquality 1 --audioquality 3 --audiobitrate 48 --speedlevel 2 --width 360 --height 240 --framerate 2 --keyint 256 --channels 1".split()
-            cmd="ffmpeg2theora --videoquality 10 --videobitrate  16778 --optimize --speedlevel 0 --audioquality 10 --audiobitrate 500 --keyint 8".split()
+            cmd="ffmpeg2theora --videoquality 10 --videobitrate  16778 --optimize --audioquality 10 --audiobitrate 500 --keyint 1".split()
             cmd+=[ '-o', dst, src ]
             print ' '.join(cmd)
             p=subprocess.Popen(cmd).wait()
