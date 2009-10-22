c55f2e0cb4d73390ce2ff672ed8a75a500a5dbe3
Carl Karsten <carl@gw42.(none)>
2009-10-22 09:06:51 -0500

    docstring typo

--- dj/scripts/enc.py
+++ dj/scripts/enc.py
@@ -53,7 +53,7 @@ def run_cmd(cmd):
 
 def mktitle(source, output_base, name, authors):
     """
-    Make a title slide by filling in a pre-make svg with name/authors.
+    Make a title slide by filling in a pre-made svg with name/authors.
     librsvg doesn't support flow, wich is needed for long titles, 
     so render it to a .png using inkscape
     """
