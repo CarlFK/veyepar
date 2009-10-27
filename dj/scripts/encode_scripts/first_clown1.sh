ba012ec27d24f216fc8d7c8ec1969f9208e6e917
Carl Karsten <carl@gw42.(none)>
2009-10-27 11:36:56 -0500

    typo, credits

--- dj/scripts/encode_scripts/first_clown1.sh
+++ dj/scripts/encode_scripts/first_clown1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 # http://www.firstclown.us/2008/07/01/transcoding-dv-to-widescreen-mp4-in-linux/
+# http://juliensimon.blogspot.com/2009/01/howto-ffmpeg-x264-presets.html
 
 # params:
 IN=$1
@@ -11,7 +12,7 @@ TEMP=$3
 ffmpeg -f dv -i $IN -pass 1 -vcodec libx264 -g 300 -vpre fastfirstpass -mbd 2 -cmp 2 -subcmp 2 -deinterlace -aspect 16:9 -s 640x360 -padtop 4 -padbottom 4 -b 768k -acodec libfaac -y $TEMP
 
 # Second Pass
-ffmpeg -f dv -i $IN -pass 2 -vcodec libx264 -vpre h -g 300 -mbd 2 -cmp 2 -subcmp 2 -deinterlace -aspect 16:9 -s 640x360 -padtop 4 -padbottom 4 -b 768k -acodec libfaac -y $TEMP
+ffmpeg -f dv -i $IN -pass 2 -vcodec libx264 -vpre hq -g 300 -mbd 2 -cmp 2 -subcmp 2 -deinterlace -aspect 16:9 -s 640x360 -padtop 4 -padbottom 4 -b 768k -acodec libfaac -y $TEMP
 
 MP4Box -add $TEMP $OUT  
 MP4Box -par 1=1:1 $OUT
