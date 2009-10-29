69907d3c887cf6f3aa0c0264cf870b32be885aac
Carl Karsten <carl@gw42.(none)>
2009-10-29 12:14:43 -0500

    refactor into smaller parts

--- dj/scripts/enc.py
+++ dj/scripts/enc.py
@@ -35,15 +35,6 @@ mlt="""
 </mlt>
 """
 
-def run_cmd(cmd):
-    print cmd
-    cmd = cmd.split()
-    print cmd
-    p=subprocess.Popen(cmd)
-    p.wait()
-    retcode=p.returncode
-    return retcode
-
 
 def time2s(time):
     """ given 's.s' or 'h:m:s.s' returns s.s """
@@ -79,15 +70,42 @@ def time2b(time,fps,bpf,default):
 class enc(process):
 
   ready_state = 2
-  
-  def mktitle(self, source, output_base, text):
+ 
+  def run_cmd(self,cmd):
+    """
+    given command list, 
+    if verbose prints stuff,
+    runs it, returns pass/fail.
+    """
+    if self.options.verbose:
+        print cmd
+    print ' '.join(cmd)
+    p=subprocess.Popen(cmd)
+    p.wait()
+    retcode=p.returncode
+    if retcode:
+        if self.options.verbose:
+            print episode.id, episode.name
+            print "transcode failed"
+            print retcode
+        ret = False
+    else:
+        ret = True
+    return ret
+ 
+  def mktitle(self, source, output_base, episode):
     """
     Make a title slide by filling in a pre-made svg with name/authors.
     melt uses librsvg which doesn't support flow, 
     wich is needed for long titles, so render it to a .png using inkscape
-    text is {'client':'PyCon', 'show':'', title, authors
     """
-
+    text={
+            'client': episode.location.show.client.name, 
+            'show': episode.location.show.name, 
+            'title': episode.name, 
+            'authors': episode.authors,
+        }
+ 
     svg_in=open(source).read()
     tree=xml.etree.ElementTree.XMLID(svg_in)
     # print tree[1]
@@ -102,39 +120,26 @@ class enc(process):
     open(cooked_svg_name,'w').write(xml.etree.ElementTree.tostring(tree[0]))
     png_name="%s.png"%output_base
     if self.options.verbose: print png_name
-    cmd="inkscape %s --export-png %s" % (cooked_svg_name, png_name)
-    run_cmd(cmd)
-
-    # dv_name="%s.dv"%output_base
-    # cmd="melt %s %s" % (png_name,dv_name)
+    cmd=["inkscape", cooked_svg_name, "--export-png", png_name]
+    self.run_cmd(cmd)
 
     return png_name
 
 
-  def melt(self,episode,cls,rfs):
+  def mkmlt(self,title_img,episode,cls,rfs):
         """
-        assemble a mlt playlist, create a title screen, 
-        call melt to mix title with playlist
-        currenly just do the first few seconds that are related to the title
-        let something else encode the rest
+        assemble a mlt playlist from:
+        mlt template, title screen image, 
+        filter parameters (currently just audio) 
+        and cutlist+raw filenames
         """
 
 # parse the xml into a tree of nodes
         tree= xml.etree.ElementTree.XMLID(mlt)
-# make a title slide
-        template = os.path.join(self.show_dir, "bling", "title.svg")
-        title_base = os.path.join(self.show_dir, "tmp", episode.slug)
-        text={
-            'client': episode.location.show.client.name, 
-            'show': episode.location.show.name, 
-            'title': episode.name, 
-            'authors': episode.authors,
-        }
-        title_name=self.mktitle(template, title_base, text)
 
 # set the title to the title slide we just made
         title=tree[1]['title']
-        title.attrib['resource']=title_name
+        title.attrib['resource']=title_img
 
 
 # get the dvfile placeholder and remove it from the tree
@@ -205,16 +210,20 @@ class enc(process):
         mlt_pathname = os.path.join(self.show_dir, "tmp", "%s.mlt"%episode.slug)
         open(mlt_pathname,'w').write(xml.etree.ElementTree.tostring(tree[0]))
 
+        return mlt_pathname
+
+  def mk_title_dv(self, mlt_pathname, episode):
+
         # make an intro .dv from title and audio
         cmd="melt -verbose -profile dv_%s %s in=0 out=149 -consumer avformat:%s pix_fmt=yuv411p" 
-        # cmd="melt -verbose -profile dv_%s %s in=0 out=149 -consumer avformat:%s f=dv pix_fmt=yuv411p" 
-        dv_pathname = os.path.join(self.show_dir, "dv", "%s.dv"%episode.slug)
-        ret = run_cmd(cmd% (self.options.format.lower(), mlt_pathname, dv_pathname))
-        # ogg_pathname = os.path.join(self.show_dir, "ogg", "%s.ogg"%episode.slug)
-        # ret = run_cmd("ffmpeg2theora %s -o %s" % ( dv_pathname, ogg_pathname))
+        dv_pathname = os.path.join(
+            self.show_dir, "tmp", "%s_title.dv"%episode.slug)
+        cmd = cmd % ( self.options.format.lower(), mlt_pathname, dv_pathname)
+        ret = self.run_cmd(cmd.split())
+# if not ret: raise something
+        return dv_pathname
 
-
-        # full lenght encodings
+  def run_melt(self, mlt_pathname, episode):
         def one_format(ext, acodec, vcodec):
             if ext in self.options.output_format:
               cmd="melt -verbose -profile dv_%s %s -consumer avformat:%s acodec=%s ab=128k ar=44100 vcodec=%s minrate=0 b=900k progressive=1 deinterlace_method=onefield" 
@@ -222,20 +231,26 @@ class enc(process):
                 self.show_dir, ext, "%s.%s"%(episode.slug,ext))
               cmd = cmd % ( self.options.format.lower(), 
                 mlt_pathname, out_pathname, acodec, vcodec)
-              ret = run_cmd(cmd)
+              ret = self.run_cmd(cmd.split())
+              if ret:
+                  if not os.exists(out_pathname):
+                      print "melt returned 0, but no output: %s" % out_pathname
+                      ret=False
+
               return ret
 
-        ret = one_format("ogg", "vorbis", "libtheora")
-        ret = one_format("flv", "libmp3lame", "flv")
-        ret = one_format("mp4", "libmp3lame", "mpeg4")
+        ret = True
+        ret = ret and one_format("ogg", "vorbis", "libtheora")
+        ret = ret and one_format("flv", "libmp3lame", "flv")
+        ret = ret and one_format("mp4", "libmp3lame", "mpeg4")
 
-        return dv_pathname
         return ret
 
-  def mkdv(self,episode,title_dv,cls,rfs):
+  def mkdv(self, mlt_pathname, episode, cls ):
         """
         assemble parts into a master .dv file
         """
+        title_dv=self.mk_title_dv(mlt_pathname, episode)
 
         # make a new dv file using just the frames to encode
         dvpathname = os.path.join(self.episode_dir,episode.slug+".dv")
@@ -243,20 +258,23 @@ class enc(process):
             print "making %s - may take awhile..." % dvpathname
         outf=open(dvpathname,'wb')
 
-# hack to splice in the intro dv make by melt()
-        inf=open(title_dv,'rb')
-        outf.write(inf.read())
-        super_hack=150*self.bpf
+        # splice in the intro dv make by melt()
+        title=open(title_dv,'rb').read()
+        title_bytes=len(title)
+        outf.write(title)
 
         for c in cls:
             if self.options.verbose: 
                 print (c.raw_file.filename, c.start,c.end)
             rawpathname = os.path.join(self.episode_dir,c.raw_file.filename)
             inf=open(rawpathname,'rb')
-            inf.seek(time2b(c.start,self.fps,self.bpf,0)+super_hack)
-            super_hack=0
+            inf.seek(time2b(c.start,self.fps,self.bpf,0)+title_bytes)
+            # there is a problem if the first clip is shorter than the title.
+# the next clip will start at 0, 
+# which is part of the title, so it will play twice.  oh well.
+            title_bytes=0 
             size=os.fstat(inf.fileno()).st_size
-            end = time2b(c.end,29.9,self.bpf,size)
+            end = time2b(c.end,self.fps,self.bpf,size)
             while inf.tell()<end:
                 outf.write(inf.read(self.bpf))
             inf.close()
@@ -268,8 +286,10 @@ class enc(process):
   def dv2theora(self,episode,title_dv,cls,rfs):
         """
         transcode dv ot ogg
+        Not much point in this now that the title side gets made
+        because now there is never the case of one .dv file
         """
-        oggpathname = os.path.join(self.show_dir, "ogg", "%s.ogg"%episode.slug)
+        oggpathname = os.path.join(self.show_dir, "theora", "%s.ogg"%episode.slug)
         cmd="ffmpeg2theora --videoquality 5 -V 600 --audioquality 5 --speedlevel 0 --optimize --keyint 256 --channels 1".split()
         cmd+=['--output',oggpathname]
 
@@ -297,38 +317,40 @@ class enc(process):
         print cl.start, cl.end
 
     if cls:
+
+# make a title slide
+        template = os.path.join(self.show_dir, "bling", "title.svg")
+        title_base = os.path.join(self.show_dir, "tmp", episode.slug)
+        title_img=self.mktitle(template, title_base, episode)
+
 # get list of raw footage for this episode
         rfs = Raw_File.objects.filter(
             cut_list__episode=episode).exclude(trash=True).distinct()
-# for now, call melt to create the title part
-        title_dv = self.melt(episode,cls,rfs)
-# maybe call a script to do the final encoding:
+
+# make a .mlt file for this episode
+        mlt = self.mkmlt(title_img,episode,cls,rfs)
+
+# do the final encoding:
+# using melt
+        self.run_melt(mlt, episode)
+  
+# using script or ffmpeg2theora
         if self.options.enc_script or "theora" in self.options.output_format:
           # consolidate all the dv files into one  
-          dvpathname = self.mkdv(episode,title_dv,cls,rfs)
+          dvpathname = self.mkdv(mlt,episode,cls)
           if self.options.enc_script:
-            # define where the script should put output and temp files
-#             out_pathname = os.path.join(
-#                 self.show_dir, "mp4", "%s.mp4"%episode.slug)
-#             temp_pathname = os.path.join(
-#                 self.show_dir, "tmp", "%s.mp4"%episode.slug)
             cmd = [self.options.enc_script, 
                     dvpathname, self.show_dir, episode.slug]
+            ret = self.run_cmd(cmd)
 
           if "theora" in self.options.output_format:
-            cmd = self.dv2theora(episode,title_dv,cls,rfs)
-
-          print ' '.join(cmd)
-          p=subprocess.Popen(cmd)
-          p.wait()
-          retcode=p.returncode
-
-        if not retcode:
-            ret = True
-        else:
-            print episode.id, episode.name
-            print "transcode failed"
-            print retcode
+              oggpathname = os.path.join(
+                self.show_dir, "theora", "%s.ogg"%episode.slug)
+              cmd="ffmpeg2theora --videoquality 5 -V 600 --audioquality 5" \
+                " --speedlevel 0 --optimize --keyint 256" \
+                " --channels 1".split()
+              cmd+=[dvpathname,'--output',oggpathname]
+              ret = self.run_cmd(cmd)
 
     else:
         print "No cutlist found."
