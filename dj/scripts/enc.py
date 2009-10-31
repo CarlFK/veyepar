4191b604e599c43f84cb6499bccd837f0db87eb0
Carl Karsten <carl@gw42.(none)>
2009-10-30 22:51:20 -0500

    write the script, don't read it

--- dj/scripts/enc.py
+++ dj/scripts/enc.py
@@ -229,7 +229,7 @@ class enc(process):
               cmd = cmd % ( self.options.format.lower(), 
                 mlt_pathname, out_pathname, acodec, vcodec)
               # write melt command out to a script:
-              open("%s.sh"%out_pathname).write(cmd)
+              open("%s.sh"%out_pathname,'w').write(cmd)
               ret = self.run_cmd(cmd.split())
               if ret:
                   if not os.path.exists(out_pathname):
