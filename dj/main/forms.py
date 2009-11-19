1db9599e0a59c1ef199cee94f6b7db651839191d
Carl Karsten <carl@gw42.(none)>
2009-11-19 01:13:07 -0600

    calendar

---
+++ dj/main/forms.py
@@ -0,0 +1,17 @@
+
+# forms.py
+
+from django import forms
+from main.models import Episode
+from django.contrib.admin import widgets                                       
+
+
+class EpisodeForm(forms.ModelForm):
+    class Meta:
+        model = Episode
+
+    def __init__(self, *args, **kwargs):
+        super(EpisodeForm, self).__init__(*args, **kwargs)
+        self.fields['start'].widget = widgets.AdminSplitDateTime()
+        self.fields['end'].widget = widgets.AdminSplitDateTime()
+
