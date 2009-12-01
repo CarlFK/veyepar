c79748bb1704fac7621c30f07a2bac0a9c9ade0d
Carl Karsten <carl@gw42.(none)>
2009-12-01 00:16:13 -0600

    change on keyprss to on blur

--- dj/static/js/fnitize.js
+++ dj/static/js/fnitize.js
@@ -1,7 +1,13 @@
 $(function(){
-	$('#id_name').keyup(function(){
-		var filename = $('#id_name').val().replace(/\s+/g, '_');
-		$('#id_slug').val(filename);
+	
+	$('#id_name').blur(function(){
+		var val = $('#id_name').val();
+		val = val.replace(/\s+/g, '_');
+		val = val.replace(/\s+/g, '_');
+		if (!$('#id_slug').val())
+        {
+            $('#id_slug').val(val);
+        }
 	})
 	
 })
