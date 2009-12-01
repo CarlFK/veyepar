e19df1e3199ec418d1e184aba6bcf17a19c05c27
Carl Karsten <carl@gw42.(none)>
2009-12-01 01:43:08 -0600

    use /media/js/urlify.js to construct filename from title

--- dj/static/js/fnitize.js
+++ dj/static/js/fnitize.js
@@ -1,13 +1,14 @@
 $(function(){
-	
-	$('#id_name').blur(function(){
-		var val = $('#id_name').val();
-		val = val.replace(/\s+/g, '_');
-		val = val.replace(/\s+/g, '_');
-		if (!$('#id_slug').val())
+    
+    $('#id_name').blur(function(){
+        var val = $('#id_name').val();
+        // val = val.replace(/\s+/g, '_');
+        val = URLify(val,300);
+        val = val.replace(/-/g, '_');
+        if (!$('#id_slug').val())
         {
             $('#id_slug').val(val);
         }
-	})
-	
+    })
+    
 })
