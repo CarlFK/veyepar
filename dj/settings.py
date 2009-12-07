bef4d8ac87bf34c6961e7126be8f1da025d964c7
Carl Karsten <carl@gw42.(none)>
2009-12-06 20:11:11 -0600

    switch from mysql to sqlite

--- dj/settings.py
+++ dj/settings.py
@@ -12,9 +12,9 @@ MANAGERS = ADMINS
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 # DATABASE_NAME = '/home/juser/dev/veyepar/dj/vp.db'             # Or path to database file if using sqlite3.
 DATABASE_NAME = 'vp.db'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'rootme'         # Not used with sqlite3.
-DATABASE_HOST = 'dell30'             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
