a1c9998569979e310dcfd24a041c090b0acf5873
Carl Karsten <carl@gw42.(none)>
2009-12-11 16:48:01 -0600

    mysql-workbench likes innodb

--- dj/settings.py
+++ dj/settings.py
@@ -17,6 +17,10 @@ DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
+#DATABASE_OPTIONS = {
+#   "init_command": "SET storage_engine=INNODB",
+#}
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
