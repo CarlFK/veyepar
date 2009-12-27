c444e7a946a93f1eca73fdbd227ac81c65bbd384
Joe User <juser@dhcp216.personnelware.com>
2009-12-26 23:35:45 -0600

    officially working, needs improvement

--- utils/gsocr.py
+++ utils/gsocr.py
@@ -16,45 +16,66 @@ import gst
 
 import gtk
  
-def one_frame(sink,buffer,pad,it):
+def Dictionary():
+    dictionary = [w for w in open('dictionary.txt').read().split() if len(w)>3]
+    return dictionary
+
+def one_frame( sink,buffer,pad, it):
+    # print len(buffer)
     if len(buffer) == 15:
         it.p = subprocess.Popen(['gocr', '-'], stdin=subprocess.PIPE, 
           stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         it.p.stdin.write(buffer)  
         it.f=open('foo.pnm','wb')
         it.f.write(buffer)  
+
+        it.skip=False ## don't seek yet, need to get the 2nd part below
+
     else:
         it.f.write(buffer)
         it.f.close()
 
         ocrtext, stderrdata = it.p.communicate(buffer)
-        print ocrtext
+
+        if it.last_ocr != ocrtext:
+            it.last_ocr=ocrtext
+            words = [w for w in ocrtext.split() if w.upper() in it.dictionary]
+            print ocrtext.__repr__()[:70]
+            # print "last", it.last_words
+            # print "cur", words
+            if it.last_words != words:
+                it.last_words = words
+                print words[:10]
+
+        it.skip=True ## flag to seek
 
         # sink.set_state(gst.STATE_NULL )
 
     return   
     
+def skip_forward(it):
+
+    if it.skip:
+        # print "in skip_forward..."
+        it.skip=False
+        pos_int = it.pipeline.query_position(it.time_format, None)[0]
+        # print "starting at", pos_int
+        seek_ns = pos_int + (10 * 1000000000)
+        it.pipeline.seek_simple(it.time_format, gst.SEEK_FLAG_FLUSH, seek_ns)
+
+    return True
+
 class Main:
 
-    def next_file(self):
-        filename=self.files.pop()
-        print filename
-        self.min,self.max = None,None
-        self.pipeline.set_state(gst.STATE_NULL)
-        self.filesrc.set_property("location", filename)
-        self.pipeline.set_state(gst.STATE_PLAYING)
-
-    def __init__(self, files):
+    def __init__(self, filename):
         
-        self.files = files
-
         pipeline = gst.Pipeline("mypipeline")
         self.pipeline=pipeline
 
 # source: file
         filesrc = gst.element_factory_make("filesrc", "audio")
         self.filesrc = filesrc
-        self.next_file()
+        self.filesrc.set_property("location", filename)
         pipeline.add(filesrc)
 
 # decoder
@@ -75,22 +96,30 @@ class Main:
         
         sink = gst.element_factory_make("fakesink", "sink")
         sink.set_property('signal-handoffs',True)
-        sink.connect('handoff',one_frame,self)
+        sink.connect('handoff', one_frame, self )
         pipeline.add(sink)
         pnmenc.link(sink)
-
-# Output images as GdkPixbuf objects in bus messages
-        # gdkpixbufsink = gst.element_factory_make("gdkpixbufsink", "gdkpixbufsink")
-        # pipeline.add(gdkpixbufsink)
-        # ffmpegcolorspace.link(gdkpixbufsink)
 
 # keep refernce to pipleline so it doesn't get destroyed 
         self.pipeline=pipeline
         pipeline.set_state(gst.STATE_PLAYING)
 
+        self.time_format = gst.Format(gst.FORMAT_TIME)
+
+        elements=list(pipeline.elements())
+        elements.reverse()
+        for e in elements:
+            print e.get_factory().get_name(),
+        print
+
         bus = pipeline.get_bus()
         bus.add_signal_watch()
-        bus.connect("message", self.on_message)
+        # bus.connect("message", self.on_message)
+
+        self.skip = False
+        self.last_ocr=''
+        self.last_words=[]
+        self.dictionary=Dictionary()
 
     def OnDynamicPad(self, dbin, pad, islast):
         print "OnDynamicPad Called!"
@@ -103,16 +132,8 @@ class Main:
         print message
         t = message.type
         print t
-        if t == gst.MESSAGE_ELEMENT \
-                and message.structure.get_name()=='pixbuf':
-            pixbuf=message.structure['pixbuf']
-            lst=[]
-            pixbuf.save_to_callback(
-                lambda b,l: l.append(b), 'bmp', user_data=lst)
-            img=''.join(lst)
-            open('foo.bmp','wb').write(img)
-
-
+        if t == gst.MESSAGE_ELEMENT:
+            pass
         elif t == gst.MESSAGE_EOS:
             # self.player.set_state(gst.STATE_NULL)
             self.next_file()
@@ -125,6 +146,6 @@ def parse_args():
 if __name__=='__main__':
     options,args = parse_args()
     gobject.threads_init()
-    if not args: args=['foo.dv']
-    p=Main(args)
+    p=Main('foo.dv')
+    gobject.idle_add( skip_forward, p, priority=gobject.PRIORITY_HIGH )
     gtk.main()
