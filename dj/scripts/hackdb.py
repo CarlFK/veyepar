043f09a0e7069e1572e515a9d7019b17ea891b6e
Tristan Sloughter <tristan@tristan-laptop.(none)>
2010-03-28 15:25:06 -0500

    adds a list of .dv files to an episode, both hardcoded.

---
+++ dj/scripts/hackdb.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+# creates cutlist items for dv files passed on the command line
+
+import os,sys
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+sys.path.insert(0, '..' )
+
+from main.models import Episode, Raw_File, Cut_List
+
+ep = Episode.objects.get(id=90)
+
+date_dir='2010-02-18'
+
+file_names="""08:52:47.dv
+08:59:44.dv
+09:06:08.dv
+10:16:11.dv
+10:33:29.dv
+11:09:37.dv
+11:10:05.dv
+11:12:40.dv
+12:16:51.dv"""
+
+seq=0
+for fn in file_names.split('\n'): 
+    pn= date_dir+'/'+fn 
+    print pn, 
+    dv = Raw_File.objects.get(filename = pn)
+    seq+=1
+
+    cl, created = Cut_List.objects.get_or_create(
+        episode=ep,
+        raw_file=dv )
+    if created:
+        cl.sequence=seq
+        cl.save()
+
