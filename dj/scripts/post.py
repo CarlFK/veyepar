d2b56fb09996cf0418fbfd6e2cafd49d7e2dec43
Carl Karsten <carl@personnelware.com>
2010-09-27 22:34:28 -0500

    dv preview defaults to users home dir

--- dj/scripts/post.py
+++ dj/scripts/post.py
@@ -157,6 +157,9 @@ Your file called Test Episode #0 has been successfully posted.
 </response>
 
 </otterResponses>
+
+'\n<?xml version="1.0" encoding="utf-8"?>\n<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"\n "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\n<html>\n  <head>\n    <title>503 Service Unavailable</title>\n  </head>\n  <body>\n    <h1>Error 503 Service Unavailable</h1>\n    <p>Service Unavailable</p>\n    <h3>Guru Meditation:</h3>\n    <p>XID: 1112821342</p>\n    <hr>\n    <p>Varnish cache server</p>\n  </body>\n</html>\n\n'
+
 """
 # (02:37:51 PM) Juhaz: CarlFK, no. tree is the root element, it can't find itself, only children.
 
