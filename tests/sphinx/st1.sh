022467d4cd2b54c30a77f4f5daa30ae5abaef615
Carl Karsten <carl@personnelware.com>
2011-04-11 19:49:53 -0500

    better test cleanup

--- tests/sphinx/st1.sh
+++ tests/sphinx/st1.sh
@@ -32,10 +32,10 @@ sox test.wav -b 16 -r 16k -e signed -c 1 -t raw test.16k
      
 # play -b 16 -r 16k -e signed -c 1 -t raw test.16k
 
+echo test>test.ctl
 HMM=/usr/share/sphinx2/model/hmm/6k
 TURT=/usr/share/sphinx2/model/lm/turtle
 TASK=.
-CTLFILE=turtle.ctl
 
 echo "using goforward.16k:    BESTPATH: GO FORWARD TEN METERS  goforward -97741561"
 
@@ -43,5 +43,5 @@ echo "using goforward.16k:    BESTPATH: GO FORWARD TEN METERS  goforward -977415
 sphinx2-continuous -verbose 9 -adcin TRUE -adcext 16k -ctlfn test.ctl -ctloffset 0 -ctlcount 100000000 -datadir ${TASK} -agcmax TRUE -langwt 6.5 -fwdflatlw 8.5 -rescorelw 9.5 -ugwt 0.5 -fillpen 1e-10 -silpen 0.005 -inspen 0.65 -top 1 -topsenfrm 3 -topsenthresh -70000 -beam 2e-06 -npbeam 2e-06 -lpbeam 2e-05 -lponlybeam 0.0005 -nwbeam 0.0005 -fwdflat FALSE -fwdflatbeam 1e-08 -fwdflatnwbeam 0.0003 -bestpath TRUE -kbdumpdir ${TASK} -lmfn ${TURT}/turtle.lm -dictfn ${TURT}/turtle.dic -ndictfn ${HMM}/noisedict -phnfn ${HMM}/phone -mapfn ${HMM}/map -hmmdir ${HMM} -hmmdirlist ${HMM} -8bsen TRUE -sendumpfn ${HMM}/sendump -cbdir ${HMM} 2>&1|grep BESTPATH
 
 
-rm test.16k test.wav test.mp4 source.txt goforward.wav
+rm test.16k test.wav test.mp4 source.txt goforward.wav test.ctl
 
