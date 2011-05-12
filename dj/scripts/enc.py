2165f3dc5937a4e5cf97e0a37d516693f3342a45
Carl Karsten <carl@personnelware.com>
2011-05-12 12:02:41 -0500

    workaround melt debug output

--- dj/scripts/enc.py
+++ dj/scripts/enc.py
@@ -224,6 +224,10 @@ class enc(process):
         p = subprocess.Popen( ['melt', mlt_pathname, '-consumer', 'xml'], 
                               stdout=subprocess.PIPE, stderr=subprocess.PIPE )
         out,err = p.communicate()
+
+        # hack to remove "Plugin 1046 exists in both ...."
+        out = '\n'.join(l for l in out.split('\n') if not l.startswith('Plugin') )
+
         t=xml.etree.ElementTree.XMLID(out)
         frames=t[1]['tractor1'].get('out')
         frames=int(frames)
