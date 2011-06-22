518429925b8ec5b0737159e6ea8c8e9ffdade26f
Carl Karsten <carl@personnelware.com>
2011-06-22 10:54:52 -0500

    add csrf to ajaxy login

--- dj/accounts/urls.py
+++ dj/accounts/urls.py
@@ -7,7 +7,7 @@ from accounts.views import logax
 # django-registration default urls
 urlpatterns = patterns(
     '',
-    url(r'^login$',
+    url(r'^login/$',
         logax,
         name='auth_login',
         ),
