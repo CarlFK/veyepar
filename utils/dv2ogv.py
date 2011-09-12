2595fd1a87f477ea6b25c3daca89dc7d8772401d
Carl Karsten <carl@personnelware.com>
2011-09-11 22:03:53 -0500

    easy ogv

---
+++ utils/dv2ogv.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+# Adds the .dv files to the raw files table
+
+import  os
+import optparse
+import subprocess
+
+def dv2ogv(dv,ogv):
+    cmd="ffmpeg2theora --videoquality 1 --audioquality 3 --audiobitrate 48 --speedlevel 2 --width 360 --keyint 256 --channels 1".split()
+    cmd+=[ '-o', ogv, dv  ]
+    print ' '.join(cmd)
+
+    p=subprocess.Popen(cmd).wait()
+
+def one_file(dv):
+    # print dv,
+    # print os.path.exists(dv)
+    p,ext = os.path.splitext(dv)
+    # print p
+    ogv = "%s.%s" % (p,'ogv')
+    if not os.path.exists(ogv):
+        dv2ogv(dv,ogv)
+    
+
+def process(dv_dir):
+    for dirpath, dirnames, filenames in os.walk(dv_dir,followlinks=True):
+        d=dirpath[len(dv_dir)+1:]
+        d=dirpath
+        # print "checking...", dirpath, d, dirnames, filenames 
+        for f in filenames:
+            if f[-3:]=='.dv':
+                one_file(os.path.join(d,f))
+
+
+def parse_args():
+    parser = optparse.OptionParser()
+    options, args = parser.parse_args()
+    return (options,args)
+
+def main():
+    options, args = parse_args()
+    process(args[0])
+
+if __name__ == "__main__":
+    main()
