4ce54064fab459ed3a6b9dc1b68a3b42004af6cc
Carl Karsten <carl@personnelware.com>
2011-11-01 23:52:56 -0500

    split pxe setup into script and list of files

--- docs/pxe-install.sh
+++ docs/pxe-install.sh
@@ -3,37 +3,36 @@
 # sudo apt-get install 
  dhcp3-server 
  tftpd-hpa
+ syslinux
  nginx
  squid-deb-proxy 
  nfs-kernel-server 
 
+# ln -s /var/lib/tftpboot/ubuntu/ /var/www/
+# ln -s /usr/lib/syslinux/ /var/lib/tftpboot/
+
+# # ln -s /usr/lib/syslinux/pxelinux.0 /var/lib/tftpboot/
+
 # http://www.memtest.org/download/4.20/memtest86+-4.20.bin.gz
-# ln -sf /var/lib/tftpboot/ubuntu/ /var/www/
-
-# preseed.cfg:
-#  d-i mirror/http/proxy string http://g2a.personnelware.com:8000/
-#  d-i time/zone string US/Central
-#  d-i passwd/username string juser
-
-
 # http://help.ubuntu.com/11.10/installation-guide/example-preseed.txt
 
 /etc/dhcp3/dhcpd.conf
 /etc/dhcp3/dhcpd.conf.macs
 
-/var/lib/tftpboot/pxelinux.0
+# /var/lib/tftpboot/pxelinux.0
+# /var/lib/tftpboot/syslinux/chain.c32
+# /var/lib/tftpboot/memdisk
 
 /var/lib/tftpboot/pxelinux.cfg/default
-/var/lib/tftpboot/syslinux/chain.c32
-/var/lib/tftpboot/memdisk
 
 /var/lib/tftpboot/util/cz/getcz.sh
 
 /var/lib/tftpboot/ubuntu/oneiric/*
-/var/lib/tftpboot/ubuntu/oneiric/getu.sh
 /var/lib/tftpboot/ubuntu/oneiric/preseed.cfg
 /var/lib/tftpboot/ubuntu/oneiric/preseed_carl.cfg
 /var/lib/tftpboot/ubuntu/oneiric/preseed_user.cfg
+/var/lib/tftpboot/ubuntu/oneiric/getu.sh
+
 /var/lib/tftpboot/ubuntu/oneiric/amd64/initrd.gz  
 /var/lib/tftpboot/ubuntu/oneiric/amd64/linux
 /var/lib/tftpboot/ubuntu/oneiric/i386/initrd.gz  
