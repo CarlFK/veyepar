69e27e9cf2e8cef9955be395e3b4d9a01d9eeeff
Carl Karsten <carl@personnelware.com>
2011-11-02 08:59:11 -0500

    pxe setup works more

--- setup/pxe/pxe-install.sh
+++ setup/pxe/pxe-install.sh
@@ -1,6 +1,10 @@
+#!/bin/bash -xe
+
 # Files needed for PXE install ubuntu
 
 # run this on target box
+
+WEBROOT=/usr/share/nginx/www
 
 sudo apt-get install  \
  dhcp3-server \
@@ -8,18 +12,22 @@ sudo apt-get install  \
  syslinux \
  nginx \
  squid-deb-proxy \
- nfs-kernel-server \ 
+ nfs-kernel-server  
 
-sudo rsync -rv carl@dc10:src/veyepar/setup/pxe/shaz /
+sudo cp -rv shaz/etc /
+sudo cp -rv shaz/var/lib/tftpboot /var/lib/
+sudo cp -rv shaz/var/www/* $WEBROOT/
 
-ln -s /var/lib/tftpboot/ubuntu/ /var/www/
-ln -s /usr/lib/syslinux/ /var/lib/tftpboot/
-ln -s syslinux/pxelinux.0 /var/lib/tftpboot/
+ln -sf /var/lib/tftpboot/ubuntu/ $WEBROOT/
+ln -sf /usr/lib/syslinux/ /var/lib/tftpboot/
+ln -sf syslinux/pxelinux.0 /var/lib/tftpboot/
 
 cd /var/lib/tftpboot/ubuntu/oneiric/
 wget -N http://help.ubuntu.com/11.10/installation-guide/example-preseed.txt
 
+mkdir -p /var/lib/tftpboot/util
 cd /var/lib/tftpboot/util
 wget http://www.memtest.org/download/4.20/memtest86+-4.20.bin.gz
 # /var/lib/tftpboot/util/cz/getcz.sh
 
+service isc-dhcp-server restart
