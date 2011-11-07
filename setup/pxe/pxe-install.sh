3ab8e4e949725b4e0e53f5038488b6b0f6f70d26
Carl Karsten <carl@personnelware.com>
2011-11-06 22:22:23 -0600

    script fixes 404lesnginx config

--- setup/pxe/pxe-install.sh
+++ setup/pxe/pxe-install.sh
@@ -7,8 +7,8 @@
 WEBPROXY=g2a:8000
 WEBROOT=/usr/share/nginx/www
 SHAZ=$(hostname)
-WEBPROXY=$SHAZ:8000
-# WEBPROXY=g2a:8000
+# WEBPROXY=$SHAZ:8000
+WEBPROXY=g2a:8000
 
 apt-get --assume-yes install  \
  dhcp3-server \
@@ -32,15 +32,27 @@ cp -rv shaz/etc/squid-deb-proxy/* /etc/squid-deb-proxy/
 # fix the different path 
 sed -i "/dhcp3/s//dhcp/"  /etc/dhcp/dhcpd.conf
 
+# 404 when the file is not found!! (duh)
+# this is the stupid line that comes from the .deb
+# try_files $uri $uri/ /index.html; 
+# and add in autoindex - cuz it is handy.
+sed -i "/^[[:space:]]*try_files \$uri \$uri\/ \/index.html;/s/.*/#cfk# &\n\t\tautoindex on;/" /etc/nginx/sites-available/default
+
+# setup ssh keys
+# make sure the server box has keys for the user
+if [ ! -f ~/.ssh/id_rsa.pub ]; then
+    ssh-keygen -f ~/.ssh/id_rsa -N ""
+fi
 # gen some keys for the nodes
 # same keys on all nodes so that any box can ssh to any other node
-mkdir -p shaz/var/www/lc/ssh
-cd shaz/var/www/lc/ssh
+cd $WEBROOT/lc/ssh
 ssh-keygen -f id_rsa -N ""
-cp id_rsa.pub authorized_keys
+chmod a+r id_rsa id_rsa.pub
 echo <<EOT >>config
 StrictHostKeyChecking no
 EOT
+cat id_rsa.pub ~/.ssh/id_rsa.pub >> authorized_keys
+cd -
 
 # put pxe boot binaries in place
 cp -r /usr/lib/syslinux/ /var/lib/tftpboot/
@@ -55,15 +67,22 @@ sed -i "/g2a.personnelware.com:8000/s//$WEBPROXY/g" $WEBROOT/ubuntu/oneiric/pres
 # get ubuntu net boot kernel/initrd
 # remove proxy for production
 http_proxy=g2a:8000 shaz/root/bin/getu.sh oneiric
+http_proxy=g2a:8000 shaz/root/bin/getu.sh precise
 
+# setup d-i preseed files
+# docs I like
+# http://www.debian.org/releases/stable/i386/apbs05.html.en
 cd $WEBROOT/ubuntu/oneiric/
 cp /usr/share/doc/installation-guide-i386/example-preseed.txt.gz .
-gunzip example-preseed.txt.gz
+gunzip --force example-preseed.txt.gz
+cd -
+
 
 mkdir -p /var/lib/tftpboot/util
 cd /var/lib/tftpboot/util
 wget -N http://www.memtest.org/download/4.20/memtest86+-4.20.bin.gz
 # /var/lib/tftpboot/util/cz/getcz.sh
+cd -
 
 service isc-dhcp-server restart
 service isc-dhcp-server stop
