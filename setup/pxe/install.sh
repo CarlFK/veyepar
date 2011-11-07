0a114e722e0c1a77b1d895ede0ee839304320f7d
Carl Karsten <carl@personnelware.com>
2011-11-07 12:54:38 -0600

    ssh into installer for debugging installer

--- setup/pxe/install.sh
+++ setup/pxe/install.sh
@@ -4,11 +4,8 @@
 
 # run this on target box
 
-WEBPROXY=g2a:8000
 WEBROOT=/usr/share/nginx/www
 SHAZ=$(hostname)
-# WEBPROXY=$SHAZ:8000
-WEBPROXY=g2a:8000
 
 apt-get --assume-yes install  \
  dhcp3-server \
@@ -22,21 +19,43 @@ apt-get --assume-yes install  \
 # convience for checking things
 ln -sf $WEBROOT
 
+
+# dhcp server:
 cp -rv shaz/etc/dhcp3/* /etc/dhcp/
-cp -rv shaz/var/lib/tftpboot/* /var/lib/tftpboot/
-cp -rv shaz/var/www/* $WEBROOT
-
-# allow ppa's, repo keys
-cp -rv shaz/etc/squid-deb-proxy/* /etc/squid-deb-proxy/
-
 # fix the different path 
 sed -i "/dhcp3/s//dhcp/"  /etc/dhcp/dhcpd.conf
+service isc-dhcp-server restart
+service isc-dhcp-server stop
 
 # 404 when the file is not found!! (duh)
 # this is the stupid line that comes from the .deb
 # try_files $uri $uri/ /index.html; 
 # and add in autoindex - cuz it is handy.
 sed -i "/^[[:space:]]*try_files \$uri \$uri\/ \/index.html;/s/.*/#cfk# &\n\t\tautoindex on;/" /etc/nginx/sites-available/default
+service nginx start
+
+# put pxe boot config and binaries in place
+cp -rv shaz/var/lib/tftpboot/* /var/lib/tftpboot/
+cp -r /usr/lib/syslinux/ /var/lib/tftpboot/
+# pxelinux.cfg/default is relitive to where it finds pxelinux.0
+# (i guess)
+ln -sf syslinux/pxelinux.0  /var/lib/tftpboot/
+
+# swap shaz for whatever this box's name is.
+sed -i "/shaz/s//$SHAZ/g" /var/lib/tftpboot/pxelinux.cfg/default
+
+# get ubuntu net boot kernel/initrd
+http_proxy=g2a:8000 shaz/root/bin/getu.sh oneiric
+http_proxy=g2a:8000 shaz/root/bin/getu.sh precise
+
+# setup d-i preseed files and scripts
+# docs I like
+# http://www.debian.org/releases/stable/i386/apbs05.html.en
+cp -rv shaz/var/www/* $WEBROOT
+cd $WEBROOT/ubuntu/oneiric/
+cp /usr/share/doc/installation-guide-i386/example-preseed.txt.gz .
+gunzip --force example-preseed.txt.gz
+cd -
 
 # setup ssh keys
 # make sure the server box has keys for the user
@@ -54,38 +73,20 @@ EOT
 cat id_rsa.pub ~/.ssh/id_rsa.pub >> authorized_keys
 cd -
 
-# put pxe boot binaries in place
-cp -r /usr/lib/syslinux/ /var/lib/tftpboot/
-# pxelinux.cfg/default is relitive to where it fins pxelinux.0
-# (i guess)
-ln -sf syslinux/pxelinux.0  /var/lib/tftpboot/
+# squid cache of install files
+# allow ppa's, repo keys
+cp -rv shaz/etc/squid-deb-proxy/* /etc/squid-deb-proxy/
+service squid-deb-proxy restart
+# set preseeed to use proxy
+# g2a is the proxy used for develment
+sed -i "/g2a.personnelware.com/s//$SHAZ/g" $WEBROOT/ubuntu/oneiric/preseed_user.cfg
 
-# swap shaz for whatever this box's name is.
-sed -i "/shaz/s//$SHAZ/g" /var/lib/tftpboot/pxelinux.cfg/default
-sed -i "/g2a.personnelware.com:8000/s//$WEBPROXY/g" $WEBROOT/ubuntu/oneiric/preseed_user.cfg
-
-# get ubuntu net boot kernel/initrd
-# remove proxy for production
-http_proxy=g2a:8000 shaz/root/bin/getu.sh oneiric
-http_proxy=g2a:8000 shaz/root/bin/getu.sh precise
-
-# setup d-i preseed files
-# docs I like
-# http://www.debian.org/releases/stable/i386/apbs05.html.en
-cd $WEBROOT/ubuntu/oneiric/
-cp /usr/share/doc/installation-guide-i386/example-preseed.txt.gz .
-gunzip --force example-preseed.txt.gz
-cd -
-
-
+# handy utilites
+# memtest
 mkdir -p /var/lib/tftpboot/util
 cd /var/lib/tftpboot/util
 wget -N http://www.memtest.org/download/4.20/memtest86+-4.20.bin.gz
+# clonezilla
 # /var/lib/tftpboot/util/cz/getcz.sh
 cd -
 
-service isc-dhcp-server restart
-service isc-dhcp-server stop
-
-service squid-deb-proxy restart
-service nginx start
--- setup/pxe/pull.sh
+++ setup/pxe/pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 
-rsync --files-from pxe-files.txt root@shaz:/ shaz
+rsync --files-from pull-files.txt root@shaz:/ shaz
 # rsync juser@g2a:/etc/squid-deb-proxy/squid-deb-proxy.conf shaz
 
