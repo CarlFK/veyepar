1fb6efacb40337dd86bee654e4fffeb2a91710cb
Carl Karsten <carl@personnelware.com>
2012-07-14 15:53:13 -0500

    add sample passwords

---
+++ dj/scripts/sample_pw.py
@@ -0,0 +1,33 @@
+blip = {'veyepar_test':'bdff6680'} # dict of user:pw 
+twit = {
+'veyepar_test':
+  { 'password':'bdff6680',
+  'consumer_key':'SOcOAwXDhMnarlzhkE2z8Q',
+  'consumer_secret':'32tcLcSK9ECqW2FEejla1uNBfFreT9GpGoQ0ingcE8s',
+  'access_key':'203869946-mzeDwKHi5Akx84zENZuaA7MLnrCqrlH98mgTlZFh',
+  'access_secret':'tnyn6YsZz3vpasUEnxEco20hCdwYxZM5mwMciDzfaU',
+} 
+   }
+bitly = {'user':'fred', 'password':'R_longstringofstuff'}
+
+yt = {
+'veyepar_test':{
+    'email': 'veyepar_test@gmail.com',
+    'password': '', 
+    'dev_key':'',
+    },
+}
+
+archive={
+        "user_name":{
+'access': "abc",
+"secret": "123"
+}
+        }   
+
+# http://richard.readthedocs.org/en/latest/admin/api.html
+pyvideo={
+        'user_name':{
+            'key':'abc'},
+        }   
+
