44c6ca22026cc547f56253ef1b61ff0acdc0fea0
Carl Karsten <carl@personnelware.com>
2012-07-26 14:51:43 -0500

    remove unneeded setting from sample pw

--- tests/you_tube/goog_auth_sample_pw.py
+++ tests/you_tube/goog_auth_sample_pw.py
@@ -1,6 +1,5 @@
 pw = {
-    'email': 'ut4ndv@gmail.com',
-    'password': '', 
-    'dev_key':'100+char long string goes here',
+    'email': 'user@gmail.com',
+    'password': 'your gog pw', 
     }
 
