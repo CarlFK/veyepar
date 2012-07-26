8a1e7a2de04c3883b4cfd05142b398db40d770f7
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-07-25 23:11:58 -0500

    Added date/time picker, tweaked episode form

--- dj/main/static/js/fnitize.js
+++ dj/main/static/js/fnitize.js
@@ -1,5 +1,5 @@
 $(function(){
-    
+
     $('#id_name').blur(function(){
         var val = $('#id_name').val();
         // val = val.replace(/\s+/g, '_');
@@ -9,6 +9,20 @@ $(function(){
         {
             $('#id_slug').val(val);
         }
-    })
-    
-})
+    });
+
+    $('#id_start').datetimepicker({
+        dateFormat: 'yy-mm-dd', // note yy is YYYY (i.e. 2009)
+        stepHour: 1,
+        stepMinute: 15,
+        timeFormat: 'hh:mm:ss',
+        showSecond: true
+    });
+
+    $('#id_duration').timepicker({
+        stepMinute: 5,
+        timeFormat: 'hh:mm:ss',
+        showSecond: true
+    });
+
+});
