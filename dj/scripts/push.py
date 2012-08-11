2d39aec436ca98d9f657af1dcf7d21f93301056a
Carl Karsten <carl@personnelware.com>
2012-08-11 17:37:28 -0500

    parameterize the data center target

--- dj/scripts/push.py
+++ dj/scripts/push.py
@@ -22,20 +22,30 @@ class push(process):
             src_pathname = os.path.join( self.show_dir, ext, "%s.%s"%(ep.slug,ext))
             files.append({'ext':ext,'pathname':src_pathname})
       
-        dest_host = 'veyepar@nextdayvideo.com'
-        dest_path = "/home/veyepar/Videos/veyepar/enthought/scipy_2012/mp4"
-        dest = "%s:%s" %( dest_host, dest_path )
-
+        # dest_host = 'veyepar@nextdayvideo.com'
+        # dest_path = "/home/veyepar/Videos/veyepar/enthought/scipy_2012/mp4"
         for f in files:
+
+            user="veyepar"
+            host =  'nextdayvideo.com'
+            dest_host = '%s@%s' % (user,host)
+            dest_path = "/home/%s/Videos/veyepar/%s/%s/%s" % (
+                    user, 
+                    ep.show.client.slug, ep.show.slug,
+                    f['ext'] )
+
+            dest = "%s:%s" %( dest_host, dest_path )
 
             cmd = ['rsync',  '-rtvP', '-e', 'ssh -p 222', 
                     f['pathname'], dest ] 
             if self.options.verbose: print cmd
-            p = subprocess.Popen( cmd )
-            ret = p.wait()
-            if self.options.verbose: print "ret:", ret
-
-        ret = True
+            if self.options.test:
+                print "testing, not coppying, returing False"
+                ret = False
+            else:
+                p = subprocess.Popen( cmd )
+                ret = p.wait()
+                if self.options.verbose: print "ret:", ret
 
         # tring to fix the db timeout problem
         # ep=Episode.objects.get(pk=ep.id)
