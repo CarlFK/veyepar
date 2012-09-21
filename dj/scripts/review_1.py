2dd36ee9eea8e2bead2452f74030780c0942b979
Carl Karsten <carl@personnelware.com>
2012-09-21 10:22:34 -0500

    helper scripts to bypass human review checks

---
+++ dj/scripts/review_1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# review_1.py - approves all the talkes in state: review1
+# hope they are good.
+
+from process import process
+
+from main.models import Show, Location, Episode, Raw_File, Cut_List
+
+class push(process):
+
+    # this will bump everything past the review1 step
+    ready_state = 6
+
+    def process_ep(self, ep):
+        if self.options.verbose: print ep.id, ep.name
+
+        ret = True
+
+        # ep.save()
+
+        return ret
+
+if __name__ == '__main__':
+    p=push()
+    p.main()
+
