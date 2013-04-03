795eac574e0b2a08078cf13aae2ea7d160f2e6ad
Carl Karsten <carl@personnelware.com>
2013-04-03 17:49:05 -0500

    melt moved to github

--- setup/melt/mkmlt.sh
+++ setup/melt/mkmlt.sh
@@ -1,19 +1,18 @@
 #!/bin/bash -x
 
-sudo apt-get install automake autoconf libtool intltool g++ yasm swig libmp3lame-dev libgavl-dev libsamplerate-dev libxml2-dev ladspa-sdk libjack-dev libsox-dev libsdl-dev libgtk2.0-dev libqt4-dev libexif-dev libtheora-dev libvdpau-dev libvorbis-dev python-dev libvpx-dev liboil0.3-dev
+sudo apt-get --assume-yes install git automake autoconf libtool intltool g++ yasm swig libmp3lame-dev libgavl-dev libsamplerate-dev libxml2-dev ladspa-sdk libjack-dev libsox-dev libsdl-dev libgtk2.0-dev liboil-dev libsoup2.4-dev libqt4-dev libexif-dev libtheora-dev libvdpau-dev libvorbis-dev python-dev kdelibs5-dev
 
-mkdir -p melted
-cd melted
-wget -N http://www.mltframework.org/twiki/pub/MLT/BuildScripts/build-melted.sh
-chmod +x build-melted.sh
+# libfaac
 
-./build-melted.sh
+mkdir -p melt
+cd melt
+wget -N https://raw.github.com/mltframework/mlt-scripts/master/build/build-melt.sh
+chmod +x build-melt.sh
+
+echo "FFMPEG_SUPPORT_FAAC=1" >> build-melt.conf
+./build-melt.sh -c build-melt.conf
 
 cd ~/bin
-ln -sf ~/melted/$(date +'%Y%m%d')/bin/melt
+ln -sf ~/melt/$(date +'%Y%m%d')/bin/melt
 cd ..
 
-exit
-source .virtualenvs/veyepar/bin/activate
-cd veyepar/dj/scripts
-./rt.sh
