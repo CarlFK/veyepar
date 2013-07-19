7c5e57f139c38acb32cae08f8cdc5f8f794c0c7f
Carl Karsten <carl@personnelware.com>
2013-07-19 15:43:09 -0500

    sample code that uses a csrf token

---
+++ tests/auth/uc.py
@@ -0,0 +1,44 @@
+# uc.py
+
+# use crsf token when logging in and get data
+
+import requests
+
+try:
+    # read credentials from a file
+    from pw import addeps 
+except ImportError:
+    # you can fill in your credentials here
+    # but better to put in pw.py so that they don't leak
+    addeps = { 
+        'pyconca2013': { 
+            'login_page':'https://2013.pycon.ca/en/account/login/',
+            'user':'test@example.com', 'password':'abc' },
+        }
+
+auth=addeps['pyconca2013']
+print auth
+
+session = requests.session()
+session.get(auth['login_page'])
+token = session.cookies['csrftoken']
+print token
+
+login_data = {
+        'username':auth['user'], 
+        'password':auth['password'], 
+        'csrfmiddlewaretoken':token,
+        'next':'/'}
+
+ret = session.post(auth['login_page'],
+        data=login_data,
+        headers={'Referer':auth['login_page']})
+
+print "ret:", ret
+
+response = session.get('https://2013.pycon.ca/en/schedule/conference.json')
+# print response.text
+j = response.json()
+print j['schedule'][0]['contact']
+
+
