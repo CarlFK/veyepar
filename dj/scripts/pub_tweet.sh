4edf2e3e038646e3773eb63acaa89bd19fb07ac5
Carl Karsten <carl@personnelware.com>
2013-11-29 22:13:10 -0800

    extract episode table to seperate file

---
+++ dj/scripts/pub_tweet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -xe
+
+if grep ^DATABASES ../local_settings.py; then
+
+/home/carl/.virtualenvs/veyepar/bin/python mk_public.py $*
+/home/carl/.virtualenvs/veyepar/bin/python tweet.py $*
+
+else
+  vim ../local_settings.py
+  exit
+fi
