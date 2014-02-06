dc33c66bb154501988ed72ee32a920e8feebb835
Tim Ansell <mithro@mithis.com>
2014-02-06 12:15:37 +1100

    Making fixunicode more robust and also fix stderr.
    Process now;
     * Actually test if unicode can be written to the terminal.
     * Only update the encoding if we need to.
     * Test the update to the encoding was successful.
    
    We now do this for both stderr and stdout too.

--- dj/scripts/fixunicode.py
+++ dj/scripts/fixunicode.py
@@ -1,8 +1,49 @@
+"""
+Module which forces stdout/stderr into a mode which allows unicode output.
+"""
 import sys
-print sys.stdout.encoding
- 
-from ctypes import pythonapi, py_object, c_char_p
-PyFile_SetEncoding = pythonapi.PyFile_SetEncoding
-PyFile_SetEncoding.argtypes = (py_object, c_char_p)
-if not PyFile_SetEncoding(sys.stdout, "UTF-8"):
-         raise ValueError
+
+try:
+    sys.stdout.write(u"\u2603")
+    sys.stdout.write(u"\b")
+except UnicodeEncodeError, e:
+    pass
+
+try:
+    sys.stderr.write(u"\u2603")
+    sys.stderr.write(u"\b")
+except UnicodeEncodeError, e:
+    pass
+
+if sys.stdout.encoding != "UTF-8" or sys.stderr.encoding != "UTF-8":
+
+    from ctypes import pythonapi, py_object, c_char_p
+    PyFile_SetEncoding = pythonapi.PyFile_SetEncoding
+    PyFile_SetEncoding.argtypes = (py_object, c_char_p)
+
+    if sys.stdout.encoding != "UTF-8":
+        if not PyFile_SetEncoding(sys.stdout, "UTF-8"):
+            raise SystemError("Unable to force stdout to UTF-8, PyFile_SetEncoding failed.")
+        
+        if sys.stdout.encoding != "UTF-8":
+            raise SystemError("Unable to force stdout to UTF-8, encoding still %s." % sys.stdout.encoding)
+
+    
+    if sys.stderr.encoding != "UTF-8":
+        if not PyFile_SetEncoding(sys.stderr, "UTF-8"):
+            raise SystemError("Unable to force stderr to UTF-8, PyFile_SetEncoding failed.")
+        
+        if sys.stderr.encoding != "UTF-8":
+            raise SystemError("Unable to force stderr to UTF-8, encoding still %s." % sys.stderr.encoding)
+
+try:
+    sys.stdout.write(u"\u2603")
+    sys.stdout.write(u"\b")
+except UnicodeEncodeError, e:
+    raise SystemError("Unable to write unicode on stdout (encoding %s).\n%s" % (sys.stdout.encoding, e))
+
+try:
+    sys.stderr.write(u"\u2603")
+    sys.stderr.write(u"\b")
+except UnicodeEncodeError, e:
+    raise SystemError("Unable to write unicode on stderr (encoding %s).\n%s" % (sys.stderr.encoding, e))
