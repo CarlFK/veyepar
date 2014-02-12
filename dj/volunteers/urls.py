973f30c34e473f7c4239e96b2b495a5da07f84c0
Jay Leadbetter <jay.leadbetter@gmail.com>
2014-02-12 14:34:22 -0700

    Working on episode review

--- dj/volunteers/urls.py
+++ dj/volunteers/urls.py
@@ -5,7 +5,8 @@ from volunteers.views import *
 
 
 urlpatterns = patterns('',
-    #(r'^$', login_required(ShowsInProcessing.as_view()), {}, 'processing_shows'),
+    #(r'^$', login_required(ShowsInProcessing.as_view()), {}, 'volunteer_show_list'),
     #(r'^(?P<show_slug>\w+)/$', login_required(ShowReview.as_view()), {}, 'volunteer_show_review'),
-    (r'^(?P<show_slug>\w+)/$', login_required(EventReview.as_view()), {}, 'volunteer_event_review'),
+    (r'^(?P<show_slug>\w+)/(?P<episode_slug>\w+)/$', login_required(EpisodeReview.as_view()), {}, 'volunteer_episode_review'),
+    (r'^(?P<show_slug>\w+)/(?P<episode_slug>\w+/(?P<edit_key>\w+)/$', EpisodeReview.as_view(), 'guest_episode_review'),
 )
--- dj/volunteers/views.py
+++ dj/volunteers/views.py
@@ -1,4 +1,7 @@
+from django.http import Http404
 from django.views.generic import TemplateView
+
+from main.models import Episode
 
 
 class ShowsInProcessing(TemplateView):
@@ -14,3 +17,27 @@ class ShowsInProcessing(TemplateView):
     
 class ShowReview(TemplateView):
     template_name = "show_review.html"
+    
+
+class EpisodeReview(TemplateView):
+    """
+    Simplified reviewing interface for volunteers
+    """
+    template_name = "episode_review.html"
+    
+    def get_context_data(self, **kwargs):
+        episode = get_object_or_404(Episode, slug=kwargs.get('episode_slug'), 
+                                    show__slug=kwargs.get('show_slug'))
+        edit_key = kwargs.get('edit_key')
+        if edit_key and not episode.edit_key == edit_key:
+            raise Http404
+        
+        # episode
+        # show
+        # same_dates (start and end time of episode)
+        # comment_form
+        return {}
+    
+    def post(self, request, *args, **kwargs):
+        from django.http import HttpResponse
+        return HttpResponse("Posted")
