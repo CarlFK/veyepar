4bf305665a9cb5d70c4512a135d0655daab63fee
Jay Leadbetter <jay.leadbetter@gmail.com>
2014-02-12 19:15:56 -0700

    Submit works on volunteer workflow page

--- dj/volunteers/forms.py
+++ dj/volunteers/forms.py
@@ -5,15 +5,28 @@ from main.models import Cut_List, Episode
 
 class EpisodeCommentForm(forms.ModelForm):
     """
-    A form only to handle Episode comments
+    A simplified form to handle Episode comments on the volunteer workflow page.
     """
+    state = forms.ChoiceField(choices=((1, 'Save and Keep Editing'), 
+                                       (2, 'Ready to Encode'),
+                                       (0, 'This Episode is Borked')), 
+                              widget=forms.RadioSelect(),
+                              label="Resolution")
+    
     class Meta:
         model = Episode
-        fields = ['id', 'comment']
+        fields = ['id', 'comment', 'state']
         exclude = ['show']
+        
+    def __init__(self, *args, **kwargs):
+        super(EpisodeCommentForm, self).__init__(*args, **kwargs)
+        self.fields['comment'].help_text = None
 
         
 class SimplifiedCutListForm(forms.ModelForm):
+    """
+    A really simplified form for volunteers to edit Cut_List instances
+    """
     apply = forms.ChoiceField(choices=((True, 'Use this video'), (False, 'Ignore')), 
                               widget=forms.RadioSelect())
     
