0304f117d9540d681a113e622b84fcd013584570
Carl Karsten <carl@personnelware.com>
2014-02-21 18:22:53 -0600

    show number of cutlist items

--- dj/scripts/assocdv.py
+++ dj/scripts/assocdv.py
@@ -11,10 +11,17 @@ from django.db.models import Q
 
 class ass_dv(process.process):
 
+    ready_state = 1
+
     # hook for run_tests to hack some values into
     cuts=[]
     
     def process_ep(self, episode):
+
+        # skip if there is already a cut list
+        if episode.cut_list_set.count():
+            return 
+
         self.cuts = mk_cuts(episode)
         print "self.cuts", self.cuts
 
