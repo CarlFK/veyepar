ae3b2b21e6969ac30bb3a6d3dd44a313c1ef0168
Carl Karsten <carl@personnelware.com>
2014-02-23 16:37:37 -0600

    remove old #code from gslevels

--- dj/scripts/gslevels.py
+++ dj/scripts/gslevels.py
@@ -12,7 +12,6 @@ import os
 
 from gi.repository import GObject, Gst, Gtk
 from gi.repository import GLib
-# GObject.threads_init()
 Gst.init(None)
 
 
@@ -25,11 +24,6 @@ class AudioPreviewer:
         uri = "file://%s" % (filename,)
 
         self.pipeline = Gst.parse_launch("uridecodebin name=decode uri=" + uri + " ! audioconvert ! level name=wavelevel interval=100000000 post-messages=true ! fakesink qos=false name=faked")
-        # faked = self.pipeline.get_by_name("faked")
-        # faked.props.sync = True
-
-        # self.nSamples = self.bElement.get_parent().get_asset().get_duration() / 1000000000  ## 1,000,000,000 is 1 seconds
-        self._level = self.pipeline.get_by_name("wavelevel")
 
         bus = self.pipeline.get_bus()
         bus.add_signal_watch()
@@ -37,10 +31,10 @@ class AudioPreviewer:
 
         self.pipeline.set_state(Gst.State.PLAYING)
 
-
         return
 
     def process(self, levs):
+        # hook for more useful things.
         self.count += 1
         print self.count, levs
         return
@@ -49,16 +43,10 @@ class AudioPreviewer:
     def _messageCb(self, bus, message):
         t = message.type
 
-        # slightly different than
-        # https://git.gnome.org/browse/pitivi/tree/pitivi/timeline/previewers.py#n867
-        # if message.src == self._level:
-        # >>> message.get_structure().get_name()=='level'
-
         if t == Gst.MessageType.ELEMENT \
               and message.has_name("level"):
 
             s = message.get_structure()
-
             try:
                 levs = [[int(i) for i in s.get_value(type)]
                     for type in ("rms","peak","decay")]
@@ -66,7 +54,6 @@ class AudioPreviewer:
 
             except ValueError as e:
                 print e
-
 
         elif t == Gst.MessageType.EOS:
             self.quit()
@@ -76,13 +63,13 @@ class AudioPreviewer:
             self.pipeline.set_state(Gst.State.NULL)
             self.mainloop.quit()
 
+# something useful
 
 # pip install pypng numpy
 # https://github.com/drj11/pypng
 
 import png
 import numpy 
-from math import sin, pi, pow
 
 class Make_png(AudioPreviewer):
 
@@ -91,17 +78,10 @@ class Make_png(AudioPreviewer):
     count = 0
     def process(self, levs):
         self.count += 1
-        # [[-48, -48], [-48, -48], [-43, -12]]
         for i in [0]:
             # color = 127*i
             color = 255
 
-            # math.pow( 10, val/20)
-            # l =pow( 10, val/20 )
-            # l = max(levs[i][0], -self.height-1)
-            # r = max( levs[i][1], -self.height-1) + self.height
-            # x = self.count/110.0 * -600 # self.height
-            # print self.count, x
             l = max( levs[i][0], -(self.height-1)) + self.height 
             r = max( levs[i][1], -(self.height-1)) + self.height * 2
             self.grid[l, self.count] = color
@@ -112,18 +92,12 @@ class Make_png(AudioPreviewer):
             for y1 in range(r+1,self.height*2):
                 self.grid[y1, self.count] = 128
 
-
 def lvlpng(file_name):
 
     p=Make_png(file_name)
     p.mainloop = GLib.MainLoop()
     p.mainloop.run()
 
-    print "p.count", p.count
-    print "len(p.grid[0])", len(p.grid[0])
-    
-    # np.resape( p.grid, (p.count,-1
-    # png.from_array(p.grid, 'L').save("test.png")
     pngname = os.path.splitext(filename)[0]+"_audio.png"
     png.from_array([row[:p.count] for row in p.grid], 'L').save(pngname)
 
@@ -147,14 +121,16 @@ def parse_args():
 
 if __name__=='__main__':
     options,args = parse_args()
-    # filename = "/home/carl/src/veyepar/dj/scripts/foo.dv"
-    # filename = "/home/carl/Videos/veyepar/test_client/test_show/mp4/Test_Episode.mp4"
-    # filename = "/home/carl/temp/Manageable_Puppet_Infrastructure.webm"
-    # filename = "/home/carl/temp/15_57_39.ogv"
 
-    filename = args[0]
+    if args:
+        filename = args[0]
+    else:
+        # filename = "/home/carl/Videos/veyepar/test_client/test_show/mp4/Test_Episode.mp4"
+        # filename = "/home/carl/temp/Manageable_Puppet_Infrastructure.webm"
+        filename = "/home/carl/temp/15_57_39.ogv"
+
     
-    # cklevels(uri)
+    # cklevels(filename)
     lvlpng(filename)
 
 
