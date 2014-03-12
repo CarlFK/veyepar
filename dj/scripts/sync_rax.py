b4d99960de3de912ee336501fa6cf9cca6c2c133
Carl Karsten <carl@personnelware.com>
2014-03-12 15:17:49 -0500

    modularize rax_sync so parts can be disabled when needed.

--- dj/scripts/sync_rax.py
+++ dj/scripts/sync_rax.py
@@ -14,7 +14,12 @@ import gslevels
 
 class SyncRax(process):
 
-    def one_file(self, show, src, dst=None):
+    def cdn_exists(self, show, dst):
+        dst = os.path.join("veyepar",show.client.slug,show.slug,dst)
+        return dst in self.names
+
+
+    def cdn_send(self, show, src, dst=None):
         """
         src is relitive to the show dir.
         src and dst get filled to full paths.
@@ -22,14 +27,14 @@ class SyncRax(process):
         if it does, try to upload it to cdn
         (rax_uploader will skip if same file exists).
         """
-        print "checking:", src
-
         if dst is None: dst = src
-
         src = os.path.join(self.show_dir,src)
         dst = os.path.join("veyepar",show.client.slug,show.slug,dst)
 
+        print "checking:", src
         if os.path.exists(src):
+
+            print "off to rax_uploader.Uploader()..."
 
             u = rax_uploader.Uploader()
 
@@ -42,63 +47,91 @@ class SyncRax(process):
 
         return
 
-    def raw_files(self, show):
-        for rf in Raw_File.objects.filter(show=show):
-            base = os.path.join(
-                    rf.location.slug, rf.basename() + "_audio.png")
-            src = os.path.join("audio_png", "dv", base)
-            self.one_file(show,src)
+    def mk_audio_png(self,src,png_name):
+        """ 
+        make audio png from source, 
+        src can be http:// or file://
+        dst is the local fs.
+        """
+        p = gslevels.Make_png()
+        p.uri = src
+        p.verbose = self.options.verbose
+        p.setup()
+        p.start()
+        ret = p.mk_png(png_name)
 
-            base = os.path.join(rf.location.slug, rf.basename() + ".ogv")
-            src = os.path.join("dv", base)
-            self.one_file(show,src)
-              
+        return ret
 
-    def sync_final(self,show,ep):
-            base = ep.slug + ".webm"
-            src = os.path.join("webm", base)
-            self.one_file(show,src)
-
-    def sync_audio_png(self,show,ep):
-            base = ep.slug + "_audio.png"
-            src = os.path.join("webm", base)
-            self.one_file(show,src)
-
-    def mk_audio_png(self,ep):
+    def mk_final_audio_png(self,ep):
         """ whack to catch up 
         if the ep doen't have a png on the local fs, 
         make it from the public webm.
         """
         png_name = os.path.join(
                     self.show_dir,"webm", ep.slug + "_audio.png")
-        if not os.path.exists(png_name):
-
-            p = gslevels.Make_png()
-            p.uri = ep.public_url
-            p.verbose = self.options.verbose
-            p.setup()
-            p.start()
-            p.mk_png(png_name)
-            ret = True
-        else:
-            ret = False
+        # if not os.path.exists(png_name):
+        ret = self.mk_audio_png(ep.public_url,png_name)
 
         return ret
 
 
-    def episodes(self, show):
-        for ep in Episode.objects.filter( show=show, state=5):
-            self.sync_final(ep)
-            ret = self.mk_audio_png(show,ep)
-            self.sync_audio_png(show,ep)
-            return
 
+    def raw_files(self, show):
+        print "getting raw files..."
+        for rf in Raw_File.objects.filter(show=show):
+
+            # look for .ogv
+            base = os.path.join(
+                    "dv", rf.location.slug, rf.basename() + ".ogv")
+            if not self.cdn_exists(show,base):
+                self.cdn_send(show,base)
+
+            # check for audio image
+            base = os.path.join(
+                    "dv", rf.location.slug, rf.basename() + "_audio.png")
+            png = os.path.join("audio_png", base)
+            if not self.cdn_exists(show,png):
+                print rf.filesize
+                src = os.path.join(self.show_dir,base)
+                dst = os.path.join(self.show_dir,png)
+                ret = self.mk_audio_png(src,dst)
+                self.cdn_send(show,png)
+              
+
+    def sync_final(self,show,ep):
+            base = os.path.join("webm", ep.slug + ".webm" )
+            if not self.cdn_exists(show,base):
+                self.cdn_send(show,base)
+
+    def sync_final_audio_png(self,show,ep):
+            base = os.path.join("webm", ep.slug + "_audio.png" )
+            if not self.cdn_exists(show,base):
+                 png_name = os.path.join( self.show_dir, base )
+                 ret = self.mk_audio_png(ep.public_url,png_name) 
+                 self.cdn_send(show,base)
+
+    def episodes(self, show):
+        for ep in Episode.objects.filter(show=show, state=5):
+            self.sync_final(show,ep)
+            self.sync_final_audio_png(show,ep)
+
+    def init_rax(self):
+         user = self.options.cloud_user
+         cf = rax_uploader.auth(user)
+         bucket_id = self.options.rax_bucket
+         container = cf.get_container(bucket_id)
+         objects = container.get_objects()
+         print "loading names..."
+         self.names = {o.name for o in objects}
+         print "loaded."
 
     def one_show(self, show):
+        self.init_rax()
         self.set_dirs(show)
 
         self.raw_files(show)
         self.episodes(show)
+
 
     def work(self):
         """
@@ -114,4 +147,5 @@ class SyncRax(process):
 if __name__=='__main__': 
     p=SyncRax()
     p.main()
+
 
