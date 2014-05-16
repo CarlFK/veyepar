0cf2d1933aae84b32585d807a2d2fd7754ed01c4
Carl Karsten <carl@personnelware.com>
2014-05-16 15:37:50 -0500

    Make bling push optional

--- setup/nodes/encode/push_confs.sh
+++ setup/nodes/encode/push_confs.sh
@@ -7,6 +7,6 @@ scp local_settings.py $1:veyepar/dj/
 cd scripts
 scp pw.py veyepar.cfg $1:veyepar/dj/scripts
 
-rsync -rtvP bling $1:veyepar/dj/scripts
+# rsync -rtvP bling $1:veyepar/dj/scripts
 
 
