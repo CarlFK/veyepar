89a694ccc267916aa48f17ca8b70c2876c51735d
Carl Karsten <carl@personnelware.com>
2014-06-24 18:22:42 -0500

    remove rsync (can use global one.)

--- dj/scripts/mk_titles.py
+++ dj/scripts/mk_titles.py
@@ -43,40 +43,6 @@ class mk_title(enc):
 
             ret = u.upload()
 
-    def rsync(self, ep, files):
-
-
-        # Ryans data center box, 
-        # veyepar user and /home dir
-        # uses ssh keys from local box 
-
-        # user="veyepar"
-        # host =  'nextdayvideo.com'
-        host = self.options.cloud_host
-        user = self.options.cloud_user
-
-        dest_host = '%s@%s' % (user,host)
-
-        dest_show_path = "/home/%s/Videos/veyepar/%s/%s" % (
-                user, ep.show.client.slug, ep.show.slug, )
-
-        for f in files:
-
-            dest_path = "%s/%s" % ( dest_show_path, f['dest_tail'] )
-
-            dest = "%s:%s" %( dest_host, dest_path )
-
-            cmd = ['ssh', '-p', '222', dest_host, 'mkdir', '-p', dest_path ]
-            self.run_cmd(cmd)
-
-            cmd = ['rsync',  
-                    '-rtvP', 
-                    '-e', 'ssh -p 222',
-                    f['src_pathname'], dest ]
-            self.run_cmd(cmd)
-
-        return 
-
     def process_ep(self, episode):
 
         title_img=self.mk_title(episode)
