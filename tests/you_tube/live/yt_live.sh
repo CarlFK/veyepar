561e2683dfbac310f2096c877dd7519ce0c66627
Carl Karsten <carl@personnelware.com>
2014-09-19 01:04:30 -0500

    trimmed down youtube live sample, included bugzilla example.

--- tests/you_tube/live/yt_live.sh
+++ tests/you_tube/live/yt_live.sh
@@ -1,6 +1,5 @@
-# https://bugzilla.gnome.org/show_bug.cgi?id=731352#c6
+#!/bin/bash -ex
 #   
-#  stormer: CarlFK: just add it before the encoder, and you'll get the stream time printed on top of your video
 # stormer: CarlFK: you don't need flashVer, videoKeyFrameRequency anymore
 # stormer: this is all legacy in snowmix
 
@@ -9,29 +8,33 @@
 # sudo apt-get install gstreamer1.0-plugins-ugly  # errored.
 # sudo aptitude -t wheezy-backports install gstreamer1.0-plugins-ugly
 # sudo apt-get install gstreamer1.0-tools gstreamer1.0-plugins-bad
+# sudo apt-get install gstreamer0.10-plugins-bad-multiverse
 
 source auth.sh
 
 gst-launch-1.0 \
-        videotestsrc pattern=18 is-live=true \
+        videotestsrc pattern=18 is-live=1 \
         ! video/x-raw, framerate=30/1, width=426, height=240 \
-        ! queue \
-        ! videoconvert \
+        ! timeoverlay \
         ! x264enc bitrate=2000 key-int-max=60 bframes=0 byte-stream=false aud=true tune=zerolatency \
         ! h264parse \
-        ! "video/x-h264,level=(string)4.1,profile=main" \
+        ! "video/x-h264,profile=main" \
         ! queue \
         ! mux. audiotestsrc is-live=true \
         ! "audio/x-raw, format=(string)S16LE, endianness=(int)1234, signed=(boolean)true, width=(int)16, depth=(int)16, rate=(int)44100, channels=(int)2" \
-        ! queue \
         ! voaacenc bitrate=128000 \
-        ! aacparse \
-        ! audio/mpeg,mpegversion=4,stream-format=raw \
-        ! queue \
         ! flvmux streamable=true name=mux \
-        ! queue \
         ! rtmpsink location="rtmp://a.rtmp.youtube.com/live2/x/$AUTH app=live2"
 
 exit
 
-gst-launch-1.0 -v --gst-debug=flvmux:0,rtmpsink:0 videotestsrc pattern=18 is-live=true ! video/x-raw, framerate=30/1, width=426, height=240 ! queue ! videoconvert ! x264enc bitrate=400 key-int-max=60 bframes=0 byte-stream=false aud=true tune=zerolatency ! h264parse ! "video/x-h264,level=(string)4.1,profile=main" ! queue ! mux. audiotestsrc is-live=true ! "audio/x-raw, format=(string)S16LE, endianness=(int)1234, signed=(boolean)true, width=(int)16, depth=(int)16, rate=(int)44100, channels=(int)2" ! queue ! voaacenc bitrate=128000 ! aacparse ! audio/mpeg,mpegversion=4,stream-format=raw ! queue ! flvmux streamable=true name=mux ! queue ! rtmpsink "location=rtmp://a.rtmp.youtube.com/live2/x/$AUTH?videoKeyframeFrequency=1&totalDatarate=4128" app=live2 flashVer="FME/3.0%20(compatible;%20FMSc%201.0)" swfUrl=rtmp://a.rtmp.youtube.com/live2
+# https://bugzilla.gnome.org/show_bug.cgi?id=731352#c6
+gst-launch-1.0 \
+    videotestsrc is-live=1 \
+        ! "video/x-raw, width=1280, height=720, framerate=30/1" \
+        ! timeoverlay \
+        ! x264enc bitrate=2000 ! "video/x-h264,profile=main" \ 
+      ! queue ! mux. \
+    audiotestsrc is-live=1 wave=12 ! faac ! queue ! mux. \
+    flvmux streamable=1 name=mux  \ 
+      ! rtmpsink location="rtmp://a.rtmp.youtube.com/live2/x/$AUTH app=live2"
