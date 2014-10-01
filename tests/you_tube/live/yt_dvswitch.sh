ca515bae3863695be11f38873d4a9b4091cb5844
Carl Karsten <carl@personnelware.com>
2014-10-01 13:13:56 -0500

    Add youtube live encoder settings specs.

--- tests/you_tube/live/yt_dvswitch.sh
+++ tests/you_tube/live/yt_dvswitch.sh
@@ -4,6 +4,21 @@
 """
 1500 Kbps 720p is too high.
 500 (480p) is good
+
+Protocol:   RTMP Flash Streaming
+Video codec:    H.264, Main 4.1
+Frame rate:     up to 60 fps
+Keyframe frequency:     2 seconds
+Audio codec:    AAC-LC (or MP3)
+Audio sample rate:  44.1 KHz
+Audio bitrate:  128 Kbps stereo
+ 
+Keyframe Frequency must be less than or equal to 2 seconds to ensure optimal transcoding on the new platform.
+Recommended advanced settings
+Pixel aspect ratio:     Square
+Frame types:    Progressive Scan, 2 B-Frames, 1 Reference Frame
+Entropy coding:     CABAC
+
 """
 
 source ~/.dvswitchrc
