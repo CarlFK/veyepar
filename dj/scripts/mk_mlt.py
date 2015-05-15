a360ffdc15cb4f80b703a588f8cad98f92efe5fc
Carl Karsten <carl@personnelware.com>
2015-05-15 13:06:34 -0500

    mk_mlt 3/4 done.

--- dj/scripts/mk_mlt.py
+++ dj/scripts/mk_mlt.py
@@ -12,7 +12,7 @@ import pprint
 def mk_mlt(template, output, params):
 
     def set_text(node,prop_name,value=None):
-        print(node_id,prop_name,value)
+        print(node,prop_name,value)
         p = node.find("property[@name='{}']".format(prop_name))
         if value is None:
             node.remove(p)
@@ -25,21 +25,22 @@ def mk_mlt(template, output, params):
     # grab nodes we are going to store values into
     nodes={}
     for id in [
-        'pl_title_img', 'tl_title',
-        'pl_foot_img', 'tl_foot',
-        'pl_vid0', 'tl_vid1',
+        'pi_title_img', 'tl_title',
+        'pi_foot_img', 'tl_foot',
+        'pl_vid0', 'pi_vid0', # Play List and Item
+        'tl_vid1', 'ti_vid1', # Time Line and Item
         'audio_fade_in', 'audio_fade_out',
         'title_fade','foot_fade',
         ]:
 
         node = tree.find(".//*[@id='{}']".format(id))
-        # import code; code.interact(local=locals())
+        print(id,node)
         nodes[id] = node
 
-    # remove placeholder nodes
+    # remove all placeholder nodes
     mlt = tree.find('.')
+
     play_list = tree.find("./playlist[@id='main bin']")
-    nodes['pe'] = play_list.find("./entry[@sample]")
     for pe in play_list.findall("./entry[@sample]"):
         producer = pe.get('producer')
         producer_node = tree.find("./producer[@id='{}']".format(producer))
@@ -47,20 +48,34 @@ def mk_mlt(template, output, params):
         play_list.remove(pe)
         mlt.remove(producer_node)
 
+    # <playlist id="playlist0">
+    # <entry producer="tl_vid1" in="00:00:00.667" out="00:00:03.003" sample="1" /> 
+    time_line = tree.find("./playlist[@id='playlist0']")
+    # import code; code.interact(local=locals())
+    for te in time_line.findall("./entry[@sample]"):
+        print("te",te)
+        producer = te.get('producer')
+        producer_node = tree.find("./producer[@id='{}']".format(producer))
+        print( "producer", producer )
+        time_line.remove(te)
+        mlt.remove(producer_node)
 
     # add each clip to the playlist
     for clip in params['clips']:
 
-        node_id = "pl_vid{}".format(clip['id'])
+        node_id = "pi_vid{}".format(clip['id'])
+        print("node_id",node_id)
 
-        pe = copy.deepcopy( nodes['pe'] )
-        pe.set("producer", node_id)
-        del pe.attrib["in"]
-        del pe.attrib["out"]
-        # set_text(pe,'length')
-        play_list.insert(0,pe)
+        # setup and add Play List
+        pl = copy.deepcopy( nodes['pl_vid0'] )
+        pl.set("producer", node_id)
+        del pl.attrib["in"]
+        del pl.attrib["out"]
+        # set_text(pl,'length')
+        play_list.insert(0,pl)
 
-        pi = copy.deepcopy( nodes['pl_vid0'] )
+        # setup and add Playlist Item
+        pi = copy.deepcopy( nodes['pi_vid0'] )
         pi.set("id", node_id)
         del pi.attrib["in"]
         del pi.attrib["out"]
@@ -69,6 +84,27 @@ def mk_mlt(template, output, params):
 
         set_text(pi,'resource',clip['filename'])
 
+    # add each cut to the timeline
+    for cut in params['cuts']:
+
+        node_id = "ti_vid{}".format(cut['id'])
+
+        tl = copy.deepcopy( nodes['tl_vid1'] )
+        tl.set("producer", node_id)
+        del tl.attrib["in"]
+        del tl.attrib["out"]
+        # set_text(pe,'length')
+        time_line.insert(0,tl)
+
+        ti = copy.deepcopy( nodes['ti_vid1'] )
+        ti.set("id", node_id)
+        del ti.attrib["in"]
+        del ti.attrib["out"]
+        # set_text(pi,'length')
+        set_text(ti,'resource',clip['filename'])
+        mlt.insert(0,ti)
+
+
     print("checking...")
 
     for pe in play_list.findall("./entry[@sample]"):
@@ -76,11 +112,11 @@ def mk_mlt(template, output, params):
         print(producer)
 
     # set title screen image
-    set_text(nodes['pl_title_img'],'resource',params['title_img'])
+    set_text(nodes['pi_title_img'],'resource',params['title_img'])
     set_text(nodes['tl_title'],'resource',params['title_img'])
 
     # set footer image
-    set_text(nodes['pl_foot_img'],'resource',params['foot_img'])
+    set_text(nodes['pi_foot_img'],'resource',params['foot_img'])
     set_text(nodes['tl_foot'],'resource',params['foot_img'])
 
     tree.write(output)
