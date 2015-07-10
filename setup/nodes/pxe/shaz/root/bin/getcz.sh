1c6fe90e3a997205101fb9a54840e4593a213a0d
Carl Karsten <carl@personnelware.com>
2015-07-10 13:04:59 -0500

    spell clonezilla exper consitantly.

--- setup/nodes/pxe/shaz/root/bin/getcz.sh
+++ setup/nodes/pxe/shaz/root/bin/getcz.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 # fstab:
-# /var/lib/tftpboot/util/cz/expr.iso /var/lib/tftpboot/util/cz/exper iso9660 loop,defaults,ro 0 0
+# /var/lib/tftpboot/util/cz/exper.iso /var/lib/tftpboot/util/cz/exper iso9660 loop,defaults,ro 0 0
 
 # http://clonezilla.org/downloads/alternative-testing/iso-zip-files.php
 
@@ -9,8 +9,8 @@ cd /var/lib/tftpboot/util/cz
 wget -N http://downloads.sourceforge.net/project/clonezilla/clonezilla_live_alternative_testing/20111025-oneiric/clonezilla-live-20111025-oneiric.iso
 
 # wget new iso
-ln -sf clonezilla-live-20111025-oneiric.iso expr.iso
-mkdir -p expr
+ln -sf clonezilla-live-20111025-oneiric.iso exper.iso
+mkdir -p exper
 
 /etc/init.d/nfs-kernel-server stop
 umount /var/lib/tftpboot/util/cz/exper
