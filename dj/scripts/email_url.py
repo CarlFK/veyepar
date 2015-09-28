fb5ed0ea066c9bdb801a95e50d78529addffbed8
Carl Karsten <carl@personnelware.com>
2015-09-28 10:32:03 -0500

    add twitter url to video URL email

--- dj/scripts/email_url.py
+++ dj/scripts/email_url.py
@@ -11,16 +11,21 @@ class email_url(email_ab):
 
     subject_template = "[{{ep.show.name}}] Video up: {{ep.name}}" 
     body_body = """
-    The video of your talk is posted:
+The video of your talk is posted:
     {{url}}
+    {% if ep.state == 7 %}
+Look at it, make sure the title is spelled right and the audio sounds reasonable.
+If you are satisfied, tweet it, blog it, whatever it.  No point in making videos if no one watches them.
 
-    Look at it, make sure the title is spelled right, let me know if it is OK.
-    If you are satisfied, tweet it, blog it, whatever it.  No point in making videos if no one watches them.
-    
-    To approve it click the Approve button at
+To approve it click the Approve button at
     http://veyepar.nextdayvideo.com/main/approve/{{ep.id}}/{{ep.slug}}/{{ep.edit_key}}/  
 
     As soon as you or someone approves your video, it will be tweeted on @NextDayVideo{% if ep.show.client.tweet_prefix %} tagged {{ep.show.client.tweet_prefix}}{% endif %}.  It will also be sent to the event organizers in hopes that they add it to the event website.  
+    {% endif %}
+    {% if ep.twitter_url %}
+It has been tweeted: {{ ep.twitter_url }}
+Re-tweet it, blog it, whatever it.  No point in making videos if no one watches them.
+    {% endif %}
     """ 
     py_name = "email_url.py"
 
