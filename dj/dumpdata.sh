1d12350022a7afe14b00391469597bfc5eac4927
Carl Karsten <carl@personnelware.com>
2017-06-27 21:36:58 -0500

    tweek backup scripts for new server.

--- dj/dumpdata.sh
+++ dj/dumpdata.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -xe
 
 python ./manage.py dumpdata >veyepar_all.json
 python ./manage.py dumpdata main >veyepar_main.json
--- dj/updb.sh
+++ dj/updb.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash -xe
 
 # updb.sh - update db
 # pulls fairly recent data from production,
-# uses a local file for auth 
+# uses a local file for auth
 # so your user/pw will be what you created for your local dev db
 # export it with:
 # ./manage.py dumpdata auth > main/fixtures/veyepar_auth.json
@@ -13,7 +13,7 @@ if grep ^DATABASES dj/local_settings.py; then
     exit
 fi
 
-# If the current user doesn't have ssh key access, 
+# If the current user doesn't have ssh key access,
 # BatchMode will cause this to fail and continue to the wget
 ssh -o BatchMode=yes veyepar@veyepar.nextdayvideo.com /home/veyepar/site/veyepar/utils/dumpdata.sh
 
@@ -24,11 +24,11 @@ SRC=http://veyepar.nextdayvideo.com/static/temp
 wget -N $SRC/veyepar_main.json
 # wget -N $SRC/veyepar_auth.json
 
-touch veyepar.db 
+touch veyepar.db
 mv  veyepar.db  ~/temp
 
 python ./manage.py migrate --noinput
-python ./manage.py loaddata veyepar_main.json 
+python ./manage.py loaddata veyepar_main.json
 python ./manage.py loaddata veyepar_auth.json
 
 # echo ./manage.py changepassword
