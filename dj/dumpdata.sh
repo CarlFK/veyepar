9f011be5cb6dea16422e41af896b2a57ecf07168
Carl Karsten <carl@personnelware.com>
2019-10-20 23:02:16 -0500

    fix bug: dumpdata auth.User > veyepar_users.json.

--- dj/dumpdata.sh
+++ dj/dumpdata.sh
@@ -2,4 +2,4 @@
 
 python ./manage.py dumpdata >veyepar_all.json
 python ./manage.py dumpdata main >veyepar_main.json
-python ./manage.py dumpdata auth.User > veyepar_all.json
+python ./manage.py dumpdata auth.User > veyepar_users.json
