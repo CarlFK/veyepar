993f0fd461eefd05fe2e91f9ab406b29a08d2f8e
Carl Karsten <carl@personnelware.com>
2023-03-18 14:19:13 -0500

    don't clone dabo if a dabo dir is in site-packages.

--- setup/ansible/roles/site/files/install_dabo.sh
+++ setup/ansible/roles/site/files/install_dabo.sh
@@ -7,7 +7,9 @@ vdir=$1
 # virtualenv dir is passed as the $1 parameter
 
 cd $(${vdir}/bin/python3 -c "import sysconfig; print( sysconfig.get_path('purelib'))")
-git clone --branch dabo3 --single-branch https://github.com/CarlFK/dabo.git dabo-master
+if [ ! -d dabo-master ]; then
+    git clone --branch dabo3 --single-branch https://github.com/CarlFK/dabo.git dabo-master
+fi
 ln -s dabo-master/dabo
 touch /home/videoteam/dabo_installed.txt
 
