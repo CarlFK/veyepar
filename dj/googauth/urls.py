2d570aa5c7cba45e1ac8d05ac0e1a535d4501d2b
Carl Karsten <carl@personnelware.com>
2023-05-15 22:56:29 -0500

    move goog auth code into utils.py

--- dj/googauth/urls.py
+++ dj/googauth/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('init/', views.goog_start, name='google_permission'),
-    path('redirect/', views.goog_redirect, name='google_redirect')
+    path('init/', views.goog_init),
+    path('redirect/', views.goog_redirect),
 ]
--- dj/googauth/views.py
+++ dj/googauth/views.py
@@ -7,98 +7,46 @@ from django.conf import settings
 from django.shortcuts import redirect
 from django.http import HttpResponse
 
-import google.oauth2.credentials
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
+from .utils import goog_start, goog_token, get_items
 
 if settings.DEBUG:
     # for dev server runing on http://localhost:8000
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-# This variable specifies the name of a file that contains the OAuth 2.0
-# information for this application, including its client_id and client_secret.
-CLIENT_SECRETS_FILE = settings.GOOG_CLIENT_SECRET
+# CLIENT_SECRET:  the OAuth 2.0 for this application, namely client_id and client_secret.
+CLIENT_SECRET_FILE = settings.GOOG_CLIENT_SECRET
+SCOPES = settings.GOOG_SCOPES # ["https://www.googleapis.com/auth/youtube.force-ssl",]
+REDIRECT_URL = settings.GOOG_REDIRECT_URL #  'http://127.0.0.1:8000/googauth/redirect/'
 
-YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube.force-ssl"
-SCOPES = [YOUTUBE_READ_WRITE_SCOPE,]
 
-REDIRECT_URL = 'http://127.0.0.1:8000/googauth/redirect/'
+def goog_init(request):
 
-def goog_start(request):
-
-    # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE,
-        scopes=SCOPES)
-
-    # The URI created here must exactly match one of the authorized redirect URIs
-    # for the OAuth 2.0 client, which you configured in the API Console. If this
-    # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
-    # error.
-    flow.redirect_uri = REDIRECT_URL
-
-    authorization_url, state = flow.authorization_url(
-        # Enable offline access so that you can refresh an access token without
-        # re-prompting the user for permission. Recommended for web server apps.
-        access_type='offline',
-        # Enable incremental authorization. Recommended as a best practice.
-        # include_granted_scopes='true',
-        )
+    authorization_url = goog_start( CLIENT_SECRET_FILE, SCOPES, REDIRECT_URL )
 
     return redirect(authorization_url)
 
 
 def goog_redirect(request):
 
-    # state created in the flow in the callback so that it can
-    # verified in the authorization server response.
+    authorization_response = request.get_full_path()
+
+    # state created in the flow in the callback will be verified
     state = request.GET['state']
 
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE,
-        scopes=SCOPES,
-        state=state)
-
-    flow.redirect_uri = REDIRECT_URL
-
-    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
-    authorization_response = request.get_full_path()
-    flow.fetch_token(authorization_response=authorization_response)
-
-    # Save credentials back to session in case access token was refreshed.
-    credentials = flow.credentials
-
-    credd = {'token': credentials.token,
-          'refresh_token': credentials.refresh_token,
-          'token_uri': credentials.token_uri,
-          'client_id': credentials.client_id,
-          'client_secret': credentials.client_secret,
-          'scopes': credentials.scopes}
+    credd = goog_token( CLIENT_SECRET_FILE, SCOPES, REDIRECT_URL, authorization_response, state)
 
     # Save Creds and bail
     # or maybe verify they can do something
 
-    api_service_name = "youtube"
-    api_version = "v3"
-
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
-
-    request = youtube.videos().list(
-            part="id",
-            chart="mostPopular",
-            prettyPrint=True
-        )
-    response = request.execute()
-
-    if not response['items']:
-        print('No data found.')
-        d = {"message": "No data found or user credentials invalid."}
-    else:
-        d = {"items": response['items']}
+    items = get_items(api_service_name="youtube", api_version="v3", credd=credd)
+    d = {
+            "authorization_response": authorization_response,
+            "credd": credd,
+            "items": items,
+            }
 
     response = HttpResponse(content_type="application/json")
-    json.dump([d, credd], response, indent=2)
+    json.dump(d, response, indent=2)
     return response
 
 
