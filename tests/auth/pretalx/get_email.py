425d8bfccbd9e6cdc1a2c722c05e5a1b54af8eb2
Carl Karsten <carl@personnelware.com>
2023-07-17 11:42:26 -0500

    failing to get email from pretalx.

---
+++ tests/auth/pretalx/get_email.py
@@ -0,0 +1,28 @@
+
+# get_email.py
+
+# docs:
+# https://docs.pretalx.org/api/fundamentals.html#authentication
+# api token stored to env pretalx_token
+# https://pretalx.northbaypython.org/nbpy-2023/me/
+
+# I should seee "email" in the keys.
+
+
+import os
+import requests
+
+from pprint import pprint
+
+url="https://pretalx.northbaypython.org/api/events/nbpy-2023/speakers/"
+api_token=os.getenv('pretalx_token')
+
+headers = {'Authorization': f'Token {api_token}'}
+
+response=requests.get( url, headers=headers )
+pprint(response.json())
+
+print(f"{response.json().keys()=}")
+
+
+
