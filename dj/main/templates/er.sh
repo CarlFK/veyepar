648e331ca0950a6da1406322ef830983abbb1d0f
Carl Karsten <carl@personnelware.com>
2023-09-26 11:38:08 -0500

    Report Editor for py3

--- dj/main/templates/er.sh
+++ dj/main/templates/er.sh
@@ -2,5 +2,6 @@
 # /home/carl/.virtualenvs/veyepar/bin/python /home/carl/.virtualenvs/veyepar/lib/python2.7/site-packages/dabo-svn/ide/ReportDesigner.py $1
 # ~/.virtualenvs/veyepar/bin/python ~/.virtualenvs/veyepar/lib/python2.7/site-packages/dabo-master/ide/ReportDesigner.py $1
 # ~/.virtualenvs/veyepar/lib/python3.5/site-packages/dabo-master/ide/ReportDesigner.py $1
+# python2 ~/.virtualenvs/test1/lib/python2.7/site-packages/dabo-master/ide/ReportDesigner.py $1
 
-python2 ~/.virtualenvs/test1/lib/python2.7/site-packages/dabo-master/ide/ReportDesigner.py $1
+~/.virtualenvs/veyepar/bin/python ~/src/dabo.ck.py3/ide/ReportDesigner.py $1
