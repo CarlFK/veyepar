81ba91f0900c50e4f4fcad76671d9c3f93985ec6
Carl Karsten <carl@personnelware.com>
2025-04-29 15:16:54 -0500

    Add yt vids to a given yt playlist.

---
+++ dj/scripts/yt_add_playlist.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+from process import process
+
+import youtube_v3_uploader
+
+import pw
+
+from django.conf import settings
+from main.models import Show, Location, Episode, Raw_File, Cut_List
+
+class mk_public(process):
+
+    ready_state = None
+
+    def up_youtube(self, ep):
+
+        uploader = youtube_v3_uploader.Uploader()
+        uploader.client_secrets_file = settings.GOOG_CLIENT_SECRET
+        uploader.token_file = pw.yt[ep.show.client.youtube_id]['filename']
+        playlist_id = ep.show.youtube_playlist_id
+        if self.options.verbose: print("Setting Youtube playlist")
+        try:
+            if playlist_id:
+                ret = uploader.add_to_playlist(ep.host_url, playlist_id)
+        # except apiclient.errors.HttpError as e:
+        except youtube_v3_uploader.HttpError as e:
+            print(e)
+            # this shouldn't happen, prolly debugging something.
+            import code
+            code.interact(local=locals())
+
+        return ret
+
+    def process_ep(self, ep):
+        # set youtube to public
+        # set richard state to live
+
+        if ep.released:
+
+            ret = True  # if something breaks, this will turn false
+
+            # don't add to playlist if there is no host_url (youtube)
+
+            if ep.host_url and ep.show.client.youtube_id:
+                ret = ret and self.up_youtube(ep)
+        else:
+
+            ret = False # Nope. Not until it is both approved and Released.
+
+
+        return ret
+
+if __name__ == '__main__':
+    p=mk_public()
+    p.main()
+
